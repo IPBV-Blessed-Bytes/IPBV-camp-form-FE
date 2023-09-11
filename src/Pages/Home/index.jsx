97aacbcb87f5f4ce65d22d3e53e9a050e378cfe3
Alvaro Leal <alvaro.leal@liferay.com>
2023-09-11 05:23:26 -0300

    Add observations to homepage

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -11,7 +11,7 @@ function FormHome(props) {
         <Container>
           <Card.Title>Bem-Vindo</Card.Title>
 
-          <div className="row mb-5">
+          <div className="row">
             <div className="col">
               <h3 className="mb-2">Acampamento no período de carnaval 2024</h3>
               <h5 className="mb-2">Igreja Presbiteriana de Boa Viagem</h5>
@@ -19,6 +19,40 @@ function FormHome(props) {
               <p className="mb-2">
                 Qualquer dúvida, favor contactar a secretaria da igreja no telefone <em>(81) 9839-0194</em>.
               </p>
+            </div>
+          </div>
+          <hr class="horizontal-line" />
+          <div className="row">
+            <div className="col">
+              <h4 className="mb-2">Observações:</h4>
+              <ul>
+                <li>
+                  <u>
+                    <b>Todos os campos do formulário devem ser preenchidos obrigatoriamente.</b>
+                  </u>
+                </li>
+                <li>
+                  Pagamentos online (aqui no formulário) são preferidos pois automatiza o processo e facilita o trabalho
+                  da secretaria da igreja.
+                </li>
+                <li>
+                  Se a opção de pagamento escolhida for presencial, ainda assim se faz necessário preenchimento do
+                  formulário para fins de controle.
+                </li>
+                <li>
+                  Para pagamentos presenciais, após finalizar o preenchimento do formulário favor se dirigir em até 7
+                  dias úteis para efetuar o pagamento.
+                </li>
+                <li>
+                  Se você tiver qualquer parente (pai, mãe, esposo, esposa, filhos, etc) que irão dormir no mesmo quarto
+                  que você, seja no colégio, hotel ou seminário, na sessão de contato nos informe isso para que possamos
+                  vincular os quartos para todos.
+                </li>
+                <li>
+                  É necessário inscrever todas as pessoas, pagantes e não pagantes, para fins de pagamento e fins de
+                  controle da secretaria. (crianças, jovens e adultos)
+                </li>
+              </ul>
             </div>
           </div>
         </Container>
