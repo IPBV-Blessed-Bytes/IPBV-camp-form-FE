9756a97511b5336a6a8c346ee2f4903891d68995
Alvaro Leal <alvaro.leal@liferay.com>
2023-09-13 20:27:58 -0300

    Update msg error

--- src/form/validations/schema.js
+++ src/form/validations/schema.js
@@ -3,24 +3,15 @@ import * as yup from 'yup';
 const personalInformationSchema = yup.object().shape({
   name: yup.string().required('Informe o seu nome'),
   birthday: yup.date().required('Informe a sua data de nascimento'),
-  rg: yup
-    .string()
-    .min(7, 'Informe um RG válido. Mínimo 7 dígitos')
-    .required('Informe o seu rg'),
-  cpf: yup
-    .string()
-    .min(11, 'Informe um CPF válido. Mínimo 11 dígitos')
-    .required('Informe o seu cpf'),
+  rg: yup.string().min(7, 'Informe um RG válido. Mínimo 7 dígitos').required('Informe o seu rg'),
+  cpf: yup.string().min(11, 'Informe um CPF válido. Mínimo 11 dígitos').required('Informe o seu cpf'),
   rgShipper: yup.string().required('Selecione o órgão expedidor do seu RG'),
   rgShipperState: yup.string().required('Selecione a UF do órgão expedidor'),
 });
 
 const additionalInformationSchema = yup.object().shape({
   email: yup.string().email('Informe um e-mail válido').required('Informe o seu e-mail'),
-  cellPhone: yup
-    .string()
-    .min(10, 'Informe um número de telefone válido')
-    .required('Informe o seu número de telefone'),
+  cellPhone: yup.string().min(10, 'Informe um número de telefone válido').required('Informe o seu número de telefone'),
   isWhatsApp: yup.boolean().required('Informe se o número é whatsapp ou não'),
   hasAllergy: yup.boolean().required('Informe se você tem alergia'),
   allergy: yup.string().when('hasAllergy', {
