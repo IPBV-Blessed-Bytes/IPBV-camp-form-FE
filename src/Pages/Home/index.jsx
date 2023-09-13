15ca644819c5f0dfaf9214b2e58af93708a8d15a
Alvaro Leal <alvaro.leal@liferay.com>
2023-09-13 20:28:28 -0300

    Update info from homepage

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -15,7 +15,8 @@ const FormHome = (props) => {
               <h5 className="mb-2">Igreja Presbiteriana de Boa Viagem</h5>
               <p className="mb-2">Siga o passo a passo para completar sua inscrição. </p>
               <p className="mb-2">
-                Qualquer dúvida, favor contactar a secretaria da igreja no telefone <em>(81) 9839-0194</em>.
+                Qualquer dúvida, favor contactar a secretaria da igreja no telefone <em>(81) 9839-0194</em> ou no email
+                <em> secretaria@inscricaoipbv.com.br</em>
               </p>
             </div>
           </div>
