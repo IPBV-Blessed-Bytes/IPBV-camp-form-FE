99df3a466cc863e542cd1353777409bf72bc2ed3
Alvaro Leal <alvaro.leal@liferay.com>
2023-09-15 00:46:51 -0300

    Update homepage text

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -13,9 +13,8 @@ const FormHome = (props) => {
             <div className="col">
               <h3 className="mb-2">Acampamento no período de carnaval 2024</h3>
               <h5 className="mb-2">Igreja Presbiteriana de Boa Viagem</h5>
-              <p className="mb-2">Siga o passo a passo para completar sua inscrição. </p>
               <p className="mb-2">
-                Qualquer dúvida, favor contactar a secretaria da igreja no telefone <em>(81) 9839-0194</em> ou no email
+              Siga o passo a passo para completar sua inscrição. Qualquer dúvida, favor contactar a secretaria da igreja no telefone <em>(81) 9839-0194</em> ou no email
                 <em> secretaria@inscricaoipbv.com.br</em>
               </p>
             </div>
