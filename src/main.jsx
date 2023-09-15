b885214b41bf3dfe58aab4920ebc04895330b959
Alvaro Leal <alvaro.leal@liferay.com>
2023-09-15 12:27:04 -0300

    Add toastify component

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,23 @@ import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter } from 'react-router-dom';
 import './styles/_Global.scss';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+    <ToastContainer
+      position="bottom-left"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={true}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss={false}
+      draggable
+      pauseOnHover
+      theme="colored"
+    />
     <App />
   </BrowserRouter>,
 );
