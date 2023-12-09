677bbcb44f84543e4b15593b8e851ca9cc503248
Alvaro Leal <alvaro.leal@liferay.com>
2023-12-09 14:55:22 -0300

    Update packages values

--- src/Pages/Packages/utils/generatePackagesValues.js
+++ src/Pages/Packages/utils/generatePackagesValues.js
@@ -65,7 +65,7 @@ const packageSeminary = ({ age, withTransportation }) => {
   }
 
   //Hospedagem
-  else if (age <= 8) {
+  if (age <= 8) {
     accomodation = [600, 0];
     accomodationDiscountDescription = 'Criança até 8 anos não paga hospedagem';
   } else if (age >= 9 && age <= 14) {
@@ -93,7 +93,7 @@ const packageSeminary = ({ age, withTransportation }) => {
 };
 
 const packageHotel = ({ age, withTransportation }) => {
-  let accomodation = [550];
+  let accomodation = [275];
   let food = [200];
   let transportation = withTransportation ? [160] : [0];
 
@@ -117,8 +117,8 @@ const packageHotel = ({ age, withTransportation }) => {
   }
 
   //Hospedagem
-  else if (age <= 10) {
-    accomodation = [550, 0];
+  if (age <= 10) {
+    accomodation = [275, 0];
     accomodationDiscountDescription = 'Criança até 10 anos não paga hospedagem *(dormindo com os pais)';
   }
 
@@ -141,10 +141,52 @@ const packageHotel = ({ age, withTransportation }) => {
   };
 };
 
+const packageOther = ({ age, withTransportation }) => {
+  let accomodation = [0];
+  let food = [280];
+  let transportation = withTransportation ? [160] : [0];
+
+  let foodDiscountDescription = '';
+  let transportationDiscountDescription = '';
+
+  //Alimentação
+  if (age <= 6) {
+    food = [280, 0];
+    foodDiscountDescription = 'Criança até 6 anos não paga alimentação';
+  } else if (age >= 7 && age <= 12) {
+    food = [280, 140];
+    foodDiscountDescription = 'Criança de 7 a 12 anos paga apenas 50% na alimentação';
+  }
+
+  //Transporte
+  if (age <= 6) {
+    transportation = withTransportation ? [160, 0] : [0];
+    transportationDiscountDescription = 'Criança até 6 anos não paga transporte *(no colo dos pais)';
+  }
+
+  const accomodationValue = accomodation.length > 1 ? accomodation[1] : accomodation[0];
+  const foodValue = food.length > 1 ? food[1] : food[0];
+  const transportationValue = transportation.length > 1 ? transportation[1] : transportation[0];
+
+  const total = accomodationValue + foodValue + transportationValue;
+
+  return {
+    total,
+    accomodation,
+    food,
+    transportation,
+    discountDescription: {
+      food: foodDiscountDescription,
+      transportation: transportationDiscountDescription,
+    },
+  };
+};
+
 const packagesBundles = {
   school: packageSchool,
   seminary: packageSeminary,
   hotel: packageHotel,
+  other: packageOther,
 };
 
 const generatePackagesValues = (packageType, age) => {
