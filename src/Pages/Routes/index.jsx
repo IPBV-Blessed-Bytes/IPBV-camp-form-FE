997a52575efaba4311c35b4f0c1b7adacaa748f7
Alvaro Leal <alvaro.leal@liferay.com>
2023-12-12 01:03:03 -0300

    Add fetchTotalRegistrations

--- src/Pages/Routes/index.jsx
+++ src/Pages/Routes/index.jsx
@@ -1,9 +1,10 @@
 import 'react-datepicker/dist/react-datepicker.css';
 import { useState, useEffect } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import { format } from 'date-fns';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
 import ChooseFormPayment from '../ChooseFormPayment';
@@ -13,17 +14,20 @@ import FormPackages from '../Packages';
 import FormPersonalData from '../PersonalData';
 import FormSuccess from '../Success';
 import Admin from '../Admin';
+import FinalReview from '../FinalReview';
 
 export const enumSteps = {
   home: 0,
   personalData: 1,
   contact: 2,
   packages: 3,
-  formPayment: 4,
-  success: 5,
+  finalReview: 4,
+  formPayment: 5,
+  success: 6,
 };
 
 const initialValues = {
+  registrationDate: '',
   personalInformation: {
     name: '',
     birthday: '',
@@ -31,6 +35,7 @@ const initialValues = {
     rg: '',
     rgShipper: '',
     rgShipperState: '',
+    gender: '',
   },
   contact: {
     cellPhone: '',
@@ -45,6 +50,7 @@ const initialValues = {
     accomodation: {
       id: '',
       name: '',
+      subAccomodation: '',
     },
     transportation: '',
     food: '',
@@ -59,11 +65,11 @@ const FormRoutes = () => {
   const [steps, setSteps] = useState(enumSteps.home);
   const [formValues, setFormValues] = useState(initialValues);
   const [formSubmitted, setFormSubmitted] = useState(false);
+  const [redirectMessage, setRedirectMessage] = useState('');
   const isNotSuccessPathname = window.location.pathname !== '/sucesso';
-  const isChurchPathname = window.location.pathname === '/igreja/admin';
+  const isChurchPathname = window.location.pathname === '/admin';
   const [availablePackages, setAvailablePackages] = useState({});
   const [loading, setLoading] = useState(false);
-  const navigateTo = useNavigate();
 
   const updateFormValues = (key) => {
     return (value) => {
@@ -124,18 +130,25 @@ const FormRoutes = () => {
 
   const sendFormValues = async () => {
     try {
-      const response = await axios.post('https://ipbv-camp-form-be-production.up.railway.app/', formValues);
+      const updatedFormValues = {
+        ...formValues,
+        registrationDate: format(new Date(), 'dd-MM-yyyy HH:mm:ss'),
+      };
+
+      const response = await axios.post('https://ipbv-camp-form-be-production.up.railway.app/', updatedFormValues);
+
       if (response.data.data.payment_url) {
         window.open(response.data.data.payment_url, '_self');
       } else if (response.status === 201) {
         setFormSubmitted(true);
-        navigateTo('/sucesso');
+        setRedirectMessage('navigate');
+        nextStep();
       }
     } catch (error) {
       const errorMessage = error.message ? error.message : String(error);
       toast.error(
-        errorMessage == 'Request failed with status code 403'
-          ? 'CPF já cadastrado!'
+        errorMessage === 'Request failed with status code 403'
+          ? 'CPF já Cadastrado'
           : errorMessage || 'Erro ao enviar os dados!',
       );
     } finally {
@@ -146,15 +159,35 @@ const FormRoutes = () => {
   useEffect(() => {
     const fetchPackages = async () => {
       try {
-        const response = await axios.get('https://ipbv-camp-form-be-production.up.railway.app/contagem-pacotes');
-
+        const response = await axios.get('https://ipbv-camp-form-be-production.up.railway.app/package-count');
         setAvailablePackages(response);
       } catch (error) {
-        toast.error(error.message);
+        toast.error(
+          error.message === 'Request failed with status code 503'
+            ? 'Banco de dados fora do ar. Tente novamente mais tarde'
+            : error.message,
+        );
       }
     };
 
     fetchPackages();
+  }, []);
+
+  useEffect(() => {
+    const fetchTotalRegistrations = async () => {
+      try {
+        const response = await axios.get('https://ipbv-camp-form-be-production.up.railway.app/total-registrations');
+        console.log('fetchTotalRegistrations', response);
+      } catch (error) {
+        toast.error(
+          error.message === 'Request failed with status code 503'
+            ? 'Banco de dados fora do ar. Tente novamente mais tarde'
+            : error.message,
+        );
+      }
+    };
+
+    fetchTotalRegistrations();
   }, []);
 
   return (
@@ -200,6 +233,16 @@ const FormRoutes = () => {
             />
           )}
 
+          {steps === enumSteps.finalReview && isNotSuccessPathname && (
+            <FinalReview
+              nextStep={nextStep}
+              backStep={backStep}
+              updateForm={updateFormValues('finalReview')}
+              formValues={formValues}
+              redirectMessage={redirectMessage}
+            />
+          )}
+
           {steps === enumSteps.formPayment && isNotSuccessPathname && (
             <ChooseFormPayment
               initialValues={formValues}
@@ -227,7 +270,7 @@ const FormRoutes = () => {
         <Footer />
       </div>
       <Routes>
-        <Route path="/igreja/admin" element={<Admin />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </div>
   );
