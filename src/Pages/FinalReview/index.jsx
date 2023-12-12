f288ee0999ac17acb4bff8bfbc0fef99d02080c8
Alvaro Leal <alvaro.leal@liferay.com>
2023-12-12 03:55:42 -0300

    Update Final Review component

--- src/Pages/FinalReview/index.jsx
+++ src/Pages/FinalReview/index.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-
+import { useNavigate, useLocation } from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
@@ -8,142 +7,141 @@ import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
-const FinalReview = ({ nextStep, backStep, formValues, redirectMessage }) => {
+const FinalReview = ({ nextStep, backStep, formValues, sendForm }) => {
   const [isConfirmed, setIsConfirmed] = useState(false);
   const navigateTo = useNavigate();
+  const location = useLocation();
 
   const handleCheckboxChange = (e) => {
     setIsConfirmed(e.target.checked);
   };
 
   const handleClick = () => {
-    if (redirectMessage === 'navigate') {
-      console.log('entrou no 1');
+    if (formValues.package.price === 0) {
+      sendForm();
       navigateTo('/sucesso');
     } else {
-      console.log('entrou aqui');
       nextStep();
     }
   };
 
+  const isSuccessPathname = location.pathname === '/sucesso';
+
   return (
     <>
-      {window.location.href !== '/sucesso' ||
-        (window.location.href === '/sucesso' && redirectMessage === 'navigate' ? (
-          <Card className="form__container__general-height">
-            <Card.Body>
-              <Container>
-                <div className="form-review">
-                  <Card.Title>Revisão de Dados</Card.Title>
-                  <Card.Text>Revise os dados do formulário antes de submeter.</Card.Text>
+      {!isSuccessPathname && (
+        <Card className="form__container__general-height">
+          <Card.Body>
+            <Container>
+              <div className="form-review">
+                <Card.Title>Revisão de Dados</Card.Title>
+                <Card.Text>Revise os dados do formulário antes de submeter.</Card.Text>
 
-                  <Form>
-                    <Row>
-                      <Col md={6} className=" fw-bold">
-                        <Card.Text>
-                          <span className="form-review__section-title">Nome:</span> <br />
-                          {formValues.personalInformation.name}
-                        </Card.Text>
-                      </Col>
-                      <Col md={6} className=" fw-bold">
-                        <Card.Text>
-                          <span className="form-review__section-title">Pacote:</span> <br />
-                          Nome - {formValues.package.accomodation.name}
-                          <br />
-                          Preço - R$ {formValues.package.price},00
-                        </Card.Text>
-                      </Col>
-                    </Row>{' '}
-                    <div className="packages-horizontal-line" />
-                    <Row>
-                      <Col md={6} className=" fw-bold">
-                        <Card.Text>
-                          <span className="form-review__section-title">Gênero:</span> <br />
-                          {formValues.personalInformation.gender}
-                        </Card.Text>
-                      </Col>
-                      <Col md={6} className=" fw-bold">
-                        <Card.Text>
-                          <span className="form-review__section-title"> Data de Nascimento:</span> <br />{' '}
-                          {formValues.personalInformation.birthday.toISOString().split('T')[0]}
-                        </Card.Text>
-                      </Col>
-                    </Row>{' '}
-                    <div className="packages-horizontal-line" />
-                    <Row>
-                      <Col md={6} className=" fw-bold">
-                        <Card.Text>
-                          <span className="form-review__section-title"> CPF:</span> <br />{' '}
-                          {formValues.personalInformation.cpf}
-                        </Card.Text>
-                      </Col>
-                      <Col md={6} className=" fw-bold">
-                        <Card.Text>
-                          <span className="form-review__section-title">RG:</span> <br />
-                          {formValues.personalInformation.rg}
-                        </Card.Text>
-                      </Col>
-                    </Row>{' '}
-                    <div className="packages-horizontal-line" />
-                    <Row>
-                      <Col md={6} className=" fw-bold">
-                        <Card.Text>
-                          <span className="form-review__section-title"> Telefone: </span>
-                          <br />
-                          {formValues.contact.cellPhone} - Whatsapp ({formValues.contact.isWhatsApp ? 'Sim' : 'Não'})
-                        </Card.Text>
-                      </Col>
-                      <Col md={6} className=" fw-bold">
-                        <Card.Text>
-                          <span className="form-review__section-title"> Email: </span>
-                          <br />
-                          {formValues.contact.email}
-                        </Card.Text>
-                      </Col>
-                    </Row>{' '}
-                    <div className="packages-horizontal-line" />
-                    <Row>
-                      <Col md={6} className=" fw-bold">
-                        <Card.Text>
-                          <span className="form-review__section-title">Alergia: </span>
-                          <br />
-                          {formValues.contact.hasAllergy ? 'Sim -' : 'Não'} {formValues.contact.allergy}
-                        </Card.Text>
-                      </Col>
-                      <Col md={6} className=" fw-bold">
-                        <Card.Text>
-                          <span className="form-review__section-title"> Agregados:</span> <br />{' '}
-                          {formValues.contact.hasAggregate ? 'Sim -' : 'Não'} {formValues.contact.aggregate}
-                        </Card.Text>
-                      </Col>
-                    </Row>{' '}
-                    <div className="packages-horizontal-line" />
-                    <Form.Group className="d-flex justify-content-center">
-                      <Form.Check
-                        className="form-review__section-title fw-bold"
-                        type={'checkbox'}
-                        label={'Os valores de preenchimento estão corretos?'}
-                        onChange={handleCheckboxChange}
-                        checked={isConfirmed}
-                      />
-                    </Form.Group>
-                  </Form>
-                </div>
-              </Container>
-            </Card.Body>
+                <Form>
+                  <Row>
+                    <Col md={6} className=" fw-bold">
+                      <Card.Text>
+                        <span className="form-review__section-title">Nome:</span> <br />
+                        {formValues.personalInformation.name}
+                      </Card.Text>
+                    </Col>
+                    <Col md={6} className=" fw-bold">
+                      <Card.Text>
+                        <span className="form-review__section-title">Pacote:</span> <br />
+                        Nome - {formValues.package.accomodation.name}
+                        <br />
+                        Preço - R$ {formValues.package.price},00
+                      </Card.Text>
+                    </Col>
+                  </Row>{' '}
+                  <div className="packages-horizontal-line" />
+                  <Row>
+                    <Col md={6} className=" fw-bold">
+                      <Card.Text>
+                        <span className="form-review__section-title">Gênero:</span> <br />
+                        {formValues.personalInformation.gender}
+                      </Card.Text>
+                    </Col>
+                    <Col md={6} className=" fw-bold">
+                      <Card.Text>
+                        <span className="form-review__section-title"> Data de Nascimento:</span> <br />{' '}
+                        {formValues.personalInformation.birthday.toISOString().split('T')[0]}
+                      </Card.Text>
+                    </Col>
+                  </Row>{' '}
+                  <div className="packages-horizontal-line" />
+                  <Row>
+                    <Col md={6} className=" fw-bold">
+                      <Card.Text>
+                        <span className="form-review__section-title"> CPF:</span> <br />{' '}
+                        {formValues.personalInformation.cpf}
+                      </Card.Text>
+                    </Col>
+                    <Col md={6} className=" fw-bold">
+                      <Card.Text>
+                        <span className="form-review__section-title">RG:</span> <br />
+                        {formValues.personalInformation.rg}
+                      </Card.Text>
+                    </Col>
+                  </Row>{' '}
+                  <div className="packages-horizontal-line" />
+                  <Row>
+                    <Col md={6} className=" fw-bold">
+                      <Card.Text>
+                        <span className="form-review__section-title"> Telefone: </span>
+                        <br />
+                        {formValues.contact.cellPhone} - Whatsapp ({formValues.contact.isWhatsApp ? 'Sim' : 'Não'})
+                      </Card.Text>
+                    </Col>
+                    <Col md={6} className=" fw-bold">
+                      <Card.Text>
+                        <span className="form-review__section-title"> Email: </span>
+                        <br />
+                        {formValues.contact.email}
+                      </Card.Text>
+                    </Col>
+                  </Row>{' '}
+                  <div className="packages-horizontal-line" />
+                  <Row>
+                    <Col md={6} className=" fw-bold">
+                      <Card.Text>
+                        <span className="form-review__section-title">Alergia: </span>
+                        <br />
+                        {formValues.contact.hasAllergy ? 'Sim -' : 'Não'} {formValues.contact.allergy}
+                      </Card.Text>
+                    </Col>
+                    <Col md={6} className=" fw-bold">
+                      <Card.Text>
+                        <span className="form-review__section-title"> Agregados:</span> <br />{' '}
+                        {formValues.contact.hasAggregate ? 'Sim -' : 'Não'} {formValues.contact.aggregate}
+                      </Card.Text>
+                    </Col>
+                  </Row>{' '}
+                  <div className="packages-horizontal-line" />
+                  <Form.Group className="d-flex justify-content-center">
+                    <Form.Check
+                      className="form-review__section-title fw-bold"
+                      type={'checkbox'}
+                      label={'Os valores de preenchimento estão corretos?'}
+                      onChange={handleCheckboxChange}
+                      checked={isConfirmed}
+                    />
+                  </Form.Group>
+                </Form>
+              </div>
+            </Container>
+          </Card.Body>
 
-            <div className="form__container__buttons">
-              <Button variant="light" onClick={backStep} size="lg">
-                Voltar
-              </Button>
-              <Button variant="warning" onClick={handleClick} size="lg" disabled={!isConfirmed}>
-                CONFIRMAR
-              </Button>
-            </div>
-          </Card>
-        ) : (
-          ''
-        ))}
+          <div className="form__container__buttons">
+            <Button variant="light" onClick={backStep} size="lg">
+              Voltar
+            </Button>
+            <Button variant="warning" onClick={handleClick} size="lg" disabled={!isConfirmed}>
+              CONFIRMAR
+            </Button>
+          </div>
+        </Card>
+      )}
     </>
   );
 };
