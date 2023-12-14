bc8efaa584f12db31fda38ca6d765f13cea5dd9a
Alvaro Leal <alvaro.leal@liferay.com>
2023-12-14 02:20:40 -0300

    Update header to toggle sites more efficiently

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
+import Offline from './Pages/Offline';
 import FormRoutes from './Pages/Routes';
 
 function App() {
-  const isToUseGoogleForm = true;
-  return isToUseGoogleForm ? (
-    <div
-      style={{
-        width: '100vw',
-        height: '100vh',
-        overflowX: 'hidden',
-      }}
-    >
-      <iframe
-        src="https://docs.google.com/forms/d/e/1FAIpQLSdfSPsDeRU7P5gP0Rv7IXlFB_C4VoCP7JZEcban-Za8qiKmQw/viewform?embedded=true"
-        width="100%"
-        height="100%"
-        frameBorder="0"
-        marginHeight="0"
-        marginWidth="0"
-      ></iframe>
-    </div>
-  ) : (
-    <FormRoutes />
+  const toggleSite = 'form-on';
+  return (
+    <>
+      {toggleSite === 'form-on' && <FormRoutes />}
+      {toggleSite === 'form-off' && <Offline />}
+      {toggleSite === 'google' && (
+        <div
+          style={{
+            width: '100vw',
+            height: '100vh',
+            overflowX: 'hidden',
+          }}
+        >
+          <iframe
+            src="https://docs.google.com/forms/d/e/1FAIpQLSdfSPsDeRU7P5gP0Rv7IXlFB_C4VoCP7JZEcban-Za8qiKmQw/viewform?embedded=true"
+            width="100%"
+            height="100%"
+            frameBorder="0"
+            marginHeight="0"
+            marginWidth="0"
+          ></iframe>
+        </div>
+      )}
+    </>
   );
 }
 
