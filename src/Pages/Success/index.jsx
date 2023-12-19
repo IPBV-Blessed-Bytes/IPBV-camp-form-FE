08f024b00764b2b00f95e33bfa48c88d8963e980
Alvaro Leal <alvaro.leal@liferay.com>
2023-12-18 21:17:35 -0300

    Add qr code payment

--- src/Pages/Success/index.jsx
+++ src/Pages/Success/index.jsx
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import qrCode from '../../../public/Images/qr-code.jpeg';
 import { Container, Card, Button } from 'react-bootstrap';
 
 const FormSuccess = ({ initialStep, resetForm, resetFormSubmitted }) => {
@@ -24,13 +25,29 @@ const FormSuccess = ({ initialStep, resetForm, resetFormSubmitted }) => {
               {pathnamePagarme ? (
                 <b>Qualquer dúvida entraremos em contato.</b>
               ) : (
-                <b>
-                  Caso a opção de pagamento escolhida tenha sido PRESENCIAL, favor entrar em contato com a secretaria em
-                  até 7 dias úteis para efetuar o pagamento. Qualquer impedimento, favor contactar a administração.
-                </b>
+                <>
+                  <b>
+                    Caso a opção de pagamento escolhida tenha sido PRESENCIAL, favor entrar em contato com a secretaria
+                    em até 7 dias úteis para efetuar o pagamento. Qualquer impedimento, favor contactar a administração.
+                  </b>
+                  <br />
+                  <br />
+                  <div className="qr-code-container">
+                    <p>
+                      Opções de pagamento presencial são cartão de crédito (maquineta) em até 12x com juros ou Pix no QR
+                      Code abaixo. <b>Enviar comprovante de transferência para o whatsapp (81) 9839-0194</b>
+                    </p>
+                    <div className="qr-code-image">
+                      <small>
+                        <em>QR Code Pix Igreja</em>
+                      </small>
+                      <img src={qrCode} />
+                    </div>
+                  </div>
+                </>
               )}
             </p>
-            <small className="mt-5">
+            <small className={`${pathnamePagarme ? 'mt-5' : ''}`}>
               <em>Igreja Presbiteriana de Boa Viagem</em>
             </small>
           </div>
