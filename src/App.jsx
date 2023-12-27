6b874ca2c5e73a1d8a767ccefbce128971e84f7e
Alvaro Leal <alvaro.leal@liferay.com>
2023-12-27 11:55:41 -0300

    Open form and update blank page view

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Offline from './Pages/Offline';
 import FormRoutes from './Pages/Routes';
 
 function App() {
-  const toggleSite = 'blank-page';
+  const toggleSite = 'form-on';
 
   console.error = (message) => {
     if (message.startsWith('Uncaught ReferenceError: originalError is not defined at App.console.error')) {
@@ -18,7 +18,9 @@ function App() {
       {toggleSite === 'form-closed' && <CloseForm />}
       {toggleSite === 'blank-page' && (
         <>
-          <b>TEMPORARIAMENTE INDISPONÍVEL. RETORNE EM BREVE!</b>
+          <b className="display-6 d-flex flex-column align-items-center px-4 mt-5">
+            TEMPORARIAMENTE INDISPONÍVEL. RETORNE EM BREVE!
+          </b>
         </>
       )}
       {toggleSite === 'google' && (
