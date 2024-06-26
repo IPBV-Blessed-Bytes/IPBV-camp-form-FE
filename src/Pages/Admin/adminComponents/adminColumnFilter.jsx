6ffe0fef7ec1cb584392cde4e4fdcba244521220
Álvaro Leal <alvaro.leal@liferay.com>
2024-06-25 23:26:45 -0300

    Add admin column filter component

---
+++ src/Pages/Admin/adminComponents/adminColumnFilter.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Icons from '../../../components/Icons';
+
+const AdminColumnFilter = ({ column }) => {
+  const filterValue = column?.filterValue || '';
+  const setFilter = column?.setFilter || (() => {});
+
+  return (
+    <div className="d-flex position-relative">
+      <input
+        className={`filter-input ${filterValue && 'actived-filter'}`}
+        value={filterValue}
+        onChange={(e) => setFilter(e.target.value || undefined)}
+        placeholder="Pesquise ..."
+      />
+      {filterValue && <Icons className="filter-icon" typeIcon="filter" iconSize={24} fill="#4267a7" />}
+    </div>
+  );
+};
+
+export default AdminColumnFilter;
+``;
