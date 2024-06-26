15a46f247625c569ffda485061a6648adf5171ec
Álvaro Leal <alvaro.leal@liferay.com>
2024-06-25 23:26:16 -0300

    Add admin logged in component

---
+++ src/Pages/Admin/adminComponents/adminLoggedIn.jsx
@@ -0,0 +1,194 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Row, Col, Card, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import AdminPackageCard from './adminPackageCard';
+import AdminExternalLinkRow from './adminExternalLinkRow';
+
+const AdminLoggedIn = ({
+  loggedInUsername,
+  handleLogout,
+  firstRowCards,
+  totalValidRegistrationsPaied,
+  totalValidRegistrationsGlobal,
+  totalRegistrations,
+  totalChildren,
+  totalNonPaied,
+  totalVacanciesWithBuses,
+}) => {
+  const navigate = useNavigate();
+
+  const handleTableClick = () => {
+    navigate('/admin/tabela');
+  };
+
+  return (
+    <>
+      <Row>
+        <Col xs={6}>
+          <Button variant="success" onClick={handleTableClick} size="lg">
+            Tabela
+          </Button>
+        </Col>
+        <Col xs={6} className="text-end mb-2">
+          <p>
+            Bem vindo(a),
+            <span>
+              <strong className="text-uppercase"> {loggedInUsername}</strong>
+            </span>
+            !
+          </p>
+          <Button variant="danger" onClick={handleLogout}>
+            Sair
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <h4 className="text-center fw-bold mb-4">PACOTES:</h4>
+        {firstRowCards.map((card) => (
+          <AdminPackageCard key={card.title} {...card} />
+        ))}
+      </Row>
+
+      <Row className="mt-4">
+        <h4 className="text-center fw-bold mb-4">TOTAL:</h4>
+      </Row>
+
+      <Row>
+        <Col className="mb-4" xs={12} md={6} lg={4}>
+          <Card className="h-100 bg-dark">
+            <Card.Body>
+              <Card.Title className="fw-bold text-warning">Total de Inscritos válidos (pagantes)</Card.Title>
+              <Card.Text>
+                Vagas Totais Preenchidas:{' '}
+                <em>
+                  <b>{isNaN(totalValidRegistrationsPaied) ? 'Indefinido' : totalValidRegistrationsPaied.toString()}</b>
+                </em>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="mb-4" xs={12} md={6} lg={4}>
+          <Card className="h-100 bg-light">
+            <Card.Body>
+              <Card.Title className="fw-bold text-success">
+                Total de Inscritos válidos <br />
+                (pagantes e não pagantes)
+              </Card.Title>
+              <Card.Text>
+                Vagas Totais Preenchidas:{' '}
+                <em>
+                  <b>
+                    {isNaN(totalValidRegistrationsGlobal) ? 'Indefinido' : totalValidRegistrationsGlobal.toString()}
+                  </b>
+                </em>
+                <br />
+                Vagas Totais Restantes:{' '}
+                <em>
+                  <b>
+                    {isNaN(375 - totalValidRegistrationsGlobal)
+                      ? 'Indefinido'
+                      : (375 - totalValidRegistrationsGlobal).toString()}
+                  </b>
+                </em>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="mb-4" xs={12} md={6} lg={4}>
+          <Card className="h-100 bg-dark">
+            <Card.Body>
+              <Card.Title className="fw-bold text-warning">
+                Total de Inscritos geral <br />
+                (adultos e crianças)
+              </Card.Title>
+              <Card.Text>
+                Vagas Totais Preenchidas:{' '}
+                <em>
+                  <b>{isNaN(totalRegistrations) ? 'Indefinido' : totalRegistrations.toString()}</b>
+                </em>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="mb-4" xs={12} md={6} lg={4}>
+          <Card className="h-100 bg-light">
+            <Card.Body>
+              <Card.Title className="fw-bold text-success">Total de Crianças</Card.Title>
+              <Card.Text>
+                Vagas Totais Preenchidas:{' '}
+                <em>
+                  <b>{isNaN(totalChildren) ? 'Indefinido' : totalChildren.toString()}</b>
+                </em>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="mb-4" xs={12} md={6} lg={4}>
+          <Card className="h-100 bg-dark">
+            <Card.Body>
+              <Card.Title className="fw-bold text-warning">Total de Não Pagantes (day use)</Card.Title>
+              <Card.Text>
+                Vagas Totais Preenchidas:{' '}
+                <em>
+                  <b>{isNaN(totalNonPaied) ? 'Indefinido' : totalNonPaied.toString()}</b>
+                </em>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col className="mb-4" xs={12} md={6} lg={4}>
+          <Card className="h-100 bg-warning">
+            <Card.Body>
+              <Card.Title className="fw-bold text-dark">Ônibus Preenchidos</Card.Title>
+              <Card.Text>
+                Vagas Preenchidas com Ônibus:{' '}
+                <em>
+                  <b>{isNaN(totalVacanciesWithBuses) ? 'Indefinido' : totalVacanciesWithBuses.toString()}</b>
+                </em>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
+      <div className="packages-horizontal-line" />
+      <h4>Notas:</h4>
+      <div>
+        <ul>
+          <li>
+            <em>
+              <b>Inscritos Válidos</b>
+            </em>{' '}
+            : Contagem de adultos que contam como uma inscrição válida
+          </li>
+          <li>
+            <em>
+              <b>Não Pagantes (day use)</b>
+            </em>{' '}
+            : Pessoas que não irão dormir, comer ou se transportar no ônibus, apenas assistir aos cultos e(ou)
+            participar das programações
+          </li>
+          <li>
+            <em>
+              <b>Total de inscritos geral</b>
+            </em>{' '}
+            : Contagem de inscritos válidos (pagantes ou não) e crianças
+          </li>
+          <li>
+            <em>
+              <b>Ônibus Preenchidos</b>
+            </em>{' '}
+            : Contagem de pessoas inscritas nos ônibus
+          </li>
+        </ul>
+      </div>
+
+      <Row>
+        <AdminExternalLinkRow />
+      </Row>
+    </>
+  );
+};
+
+export default AdminLoggedIn;
