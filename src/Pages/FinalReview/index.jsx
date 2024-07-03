6c70b6f36feaf66af7b43396fdc67597b372df16
Álvaro Leal <alvaro.leal@liferay.com>
2024-07-03 10:47:30 -0300

    Fix final review component to show data input correctly

--- src/Pages/FinalReview/index.jsx
+++ src/Pages/FinalReview/index.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
-import { format } from 'date-fns';
+import { format, isValid } from 'date-fns';
 
 const FinalReview = ({ nextStep, backStep, formValues, sendForm, status }) => {
   const [isConfirmed, setIsConfirmed] = useState(false);
@@ -25,7 +25,7 @@ const FinalReview = ({ nextStep, backStep, formValues, sendForm, status }) => {
     if (status === 'loaded') {
       navigateTo('/sucesso');
     }
-  }, [status]);
+  }, [status, navigateTo]);
 
   const isSuccessPathname = location.pathname === '/sucesso';
 
@@ -41,13 +41,13 @@ const FinalReview = ({ nextStep, backStep, formValues, sendForm, status }) => {
 
                 <Form>
                   <Row className="row-gap">
-                    <Col md={6} className=" fw-bold">
+                    <Col md={6} className="fw-bold">
                       <Card.Text>
                         <span className="form-review__section-title">Nome:</span> <br />
                         {formValues.personalInformation.name}
                       </Card.Text>
                     </Col>
-                    <Col md={6} className=" fw-bold">
+                    <Col md={6} className="fw-bold">
                       <Card.Text>
                         <span className="form-review__section-title">Pacote:</span> <br />
                         Nome - {formValues.package.accomodation.name} |{' '}
@@ -56,76 +56,78 @@ const FinalReview = ({ nextStep, backStep, formValues, sendForm, status }) => {
                         Preço - R$ {formValues.package.price},00
                       </Card.Text>
                     </Col>
-                  </Row>{' '}
+                  </Row>
                   <div className="packages-horizontal-line" />
                   <Row className="row-gap">
-                    <Col md={6} className=" fw-bold">
+                    <Col md={6} className="fw-bold">
                       <Card.Text>
                         <span className="form-review__section-title">Gênero:</span> <br />
                         {formValues.personalInformation.gender}
                       </Card.Text>
                     </Col>
-                    <Col md={6} className=" fw-bold">
+                    <Col md={6} className="fw-bold">
                       <Card.Text>
-                        <span className="form-review__section-title"> Data de Nascimento:</span> <br />{' '}
-                        {format(new Date(formValues.personalInformation.birthday), 'dd/MM/yyyy')}
+                        <span className="form-review__section-title"> Data de Nascimento:</span> <br />
+                        {isValid(new Date(formValues.personalInformation.birthday))
+                          ? format(new Date(formValues.personalInformation.birthday), 'dd/MM/yyyy')
+                          : 'Data inválida'}
                       </Card.Text>
                     </Col>
-                  </Row>{' '}
+                  </Row>
                   <div className="packages-horizontal-line" />
                   <Row className="row-gap">
-                    <Col md={6} className=" fw-bold">
+                    <Col md={6} className="fw-bold">
                       <Card.Text>
-                        <span className="form-review__section-title"> CPF:</span> <br />{' '}
+                        <span className="form-review__section-title"> CPF:</span> <br />
                         {formValues.personalInformation.cpf}
                       </Card.Text>
                     </Col>
-                    <Col md={6} className=" fw-bold">
+                    <Col md={6} className="fw-bold">
                       <Card.Text>
                         <span className="form-review__section-title">RG:</span> <br />
                         {formValues.personalInformation.rg}
                       </Card.Text>
                     </Col>
-                  </Row>{' '}
+                  </Row>
                   <div className="packages-horizontal-line" />
                   <Row className="row-gap">
-                    <Col md={6} className=" fw-bold">
+                    <Col md={6} className="fw-bold">
                       <Card.Text>
                         <span className="form-review__section-title"> Telefone: </span>
                         <br />
                         {formValues.contact.cellPhone} - Whatsapp ({formValues.contact.isWhatsApp ? 'Sim' : 'Não'})
                       </Card.Text>
                     </Col>
-                    <Col md={6} className=" fw-bold">
+                    <Col md={6} className="fw-bold">
                       <Card.Text>
                         <span className="form-review__section-title"> Email: </span>
                         <br />
                         {formValues.contact.email}
                       </Card.Text>
                     </Col>
-                  </Row>{' '}
+                  </Row>
                   <div className="packages-horizontal-line" />
                   <Row className="row-gap">
-                    <Col md={4} className=" fw-bold">
+                    <Col md={4} className="fw-bold">
                       <Card.Text>
                         <span className="form-review__section-title">Alergia: </span>
                         <br />
                         {formValues.contact.hasAllergy ? 'Sim -' : 'Não'} {formValues.contact.allergy}
                       </Card.Text>
                     </Col>
-                    <Col md={4} className=" fw-bold">
+                    <Col md={4} className="fw-bold">
                       <Card.Text>
-                        <span className="form-review__section-title"> Agregados:</span> <br />{' '}
+                        <span className="form-review__section-title"> Agregados:</span> <br />
                         {formValues.contact.hasAggregate ? 'Sim -' : 'Não'} {formValues.contact.aggregate}
                       </Card.Text>
                     </Col>
-                    <Col md={4} className=" fw-bold">
+                    <Col md={4} className="fw-bold">
                       <Card.Text>
                         <span className="form-review__section-title">Igreja:</span> <br />
                         {formValues.contact.church}
                       </Card.Text>
                     </Col>
-                  </Row>{' '}
+                  </Row>
                   <div className="packages-horizontal-line" />
                   <Form.Group className="d-flex justify-content-center">
                     <Form.Check
@@ -162,11 +164,36 @@ const FinalReview = ({ nextStep, backStep, formValues, sendForm, status }) => {
 };
 
 FinalReview.propTypes = {
-  nextStep: PropTypes.func,
-  backStep: PropTypes.func,
-  formValues: PropTypes.bool,
-  sendForm: PropTypes.func,
-  status: PropTypes.bool,
+  nextStep: PropTypes.func.isRequired,
+  backStep: PropTypes.func.isRequired,
+  formValues: PropTypes.shape({
+    personalInformation: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      gender: PropTypes.string.isRequired,
+      birthday: PropTypes.string.isRequired,
+      cpf: PropTypes.string.isRequired,
+      rg: PropTypes.string.isRequired,
+    }).isRequired,
+    package: PropTypes.shape({
+      accomodation: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        subAccomodation: PropTypes.string.isRequired,
+      }).isRequired,
+      price: PropTypes.number.isRequired,
+    }).isRequired,
+    contact: PropTypes.shape({
+      cellPhone: PropTypes.string.isRequired,
+      isWhatsApp: PropTypes.bool.isRequired,
+      email: PropTypes.string.isRequired,
+      hasAllergy: PropTypes.bool.isRequired,
+      allergy: PropTypes.string,
+      hasAggregate: PropTypes.bool.isRequired,
+      aggregate: PropTypes.string,
+      church: PropTypes.string.isRequired,
+    }).isRequired,
+  }).isRequired,
+  sendForm: PropTypes.func.isRequired,
+  status: PropTypes.string.isRequired,
 };
 
 export default FinalReview;
