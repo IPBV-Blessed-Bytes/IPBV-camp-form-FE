31de9b28e385d5bc14f20798eb09ff87816d01ca
Álvaro Leal <alvaro.leal@liferay.com>
2024-07-09 13:18:07 -0300

    Remove presential option infos in form success component

--- src/Pages/Success/index.jsx
+++ src/Pages/Success/index.jsx
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types';
-import qrCode from '../../../public/Images/qr-code.jpeg';
 import { Container, Card, Button } from 'react-bootstrap';
 
 const FormSuccess = ({ initialStep, resetForm, resetFormSubmitted }) => {
@@ -22,34 +21,7 @@ const FormSuccess = ({ initialStep, resetForm, resetFormSubmitted }) => {
             </div>
             <p className="form__success__message">Obrigado por enviar suas informações.</p>
             <p className="form__success__contact">
-              {pathnamePagarme ? (
-                <b>Qualquer dúvida entraremos em contato.</b>
-              ) : (
-                <>
-                  <b>
-                    Caso a opção de pagamento escolhida tenha sido PRESENCIAL, favor entrar em contato com a secretaria
-                    em até 7 dias úteis para efetuar o pagamento. Qualquer impedimento, favor contactar a administração.
-                  </b>
-                  <br />
-                  <br />
-                  <div className="qr-code-container">
-                    <p>
-                      Opções de pagamento presencial são cartão de crédito (maquineta) em até 12x com juros ou Pix no QR
-                      Code abaixo. <b>Enviar comprovante de transferência para o whatsapp (81) 9839-0194.</b>
-                      <br />
-                      Acrescentar 10 centavos ao pagamento para fins de controle de entradas na conta.
-                    </p>
-                    <div className="qr-code-image">
-                      <small className="text-success">
-                        <b>
-                          Chave Pix (CNPJ): <em>11.244.944/0001-20</em>
-                        </b>
-                      </small>
-                      <img src={qrCode} />
-                    </div>
-                  </div>
-                </>
-              )}
+              <b>Qualquer dúvida entraremos em contato.</b>
             </p>
             <small className={`${pathnamePagarme ? 'mt-5' : ''}`}>
               <em>Igreja Presbiteriana de Boa Viagem</em>
