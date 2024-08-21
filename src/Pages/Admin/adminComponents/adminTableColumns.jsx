6feada18ec1b98ff3a5fdb1231c5893c57dc7ad5
Álvaro Leal <alvaro.leal@liferay.com>
2024-08-21 07:13:42 -0300

    Update admin table columns component fields

--- src/Pages/Admin/adminComponents/adminTableColumns.jsx
+++ src/Pages/Admin/adminComponents/adminTableColumns.jsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AdminTableField from './adminTableField';
 import { packages } from '../../../Pages/Routes/constants';
 
-const AdminTableColumns = ({ addFormData, editFormData, handleFormChange, addForm, editForm }) => {
-  const now = new Date();
-  const day = now.getDate().toString().padStart(2, '0');
-  const month = (now.getMonth() + 1).toString().padStart(2, '0');
-  const year = now.getFullYear();
-  const formattedDate = `${day}/${month}/${year}`;
+const AdminTableColumns = ({
+  addFormData,
+  editFormData,
+  handleFormChange,
+  addForm,
+  editForm,
+  formSubmitted,
+  currentDate,
+}) => {
+  const [missingFields, setMissingFields] = useState([]);
 
   const packageOptions = packages.map((pkg) => ({
     label: pkg,
@@ -22,15 +26,24 @@ const AdminTableColumns = ({ addFormData, editFormData, handleFormChange, addFor
       name: 'package.title',
       type: 'select',
       placeholder: 'Selecione um pacote',
+      oddOrEven: 'odd',
       options: packageOptions,
+      required: true,
+      errorMessage: 'Selecione um pacote',
     },
-    { label: 'Nome', name: 'personalInformation.name', type: 'text', placeholder: 'Nome do Acampante' },
-    { label: 'Observação', name: 'observation', type: 'text', placeholder: 'Observação sobre inscrição' },
+    {
+      label: 'Nome',
+      name: 'personalInformation.name',
+      type: 'text',
+      placeholder: 'Nome do Acampante',
+      oddOrEven: 'even',
+    },
     {
       label: 'Pagamento',
       name: 'formPayment.formPayment',
       type: 'select',
       placeholder: 'Selecione status de pagamento',
+      oddOrEven: 'odd',
       options: [
         { label: 'Pago', value: 'Pago' },
         { label: 'Não Pago', value: 'Não Pago' },
@@ -41,19 +54,54 @@ const AdminTableColumns = ({ addFormData, editFormData, handleFormChange, addFor
       name: 'contact.church',
       type: 'select',
       placeholder: 'Selecione sua igreja',
+      oddOrEven: 'even',
       options: [
         { label: 'IPBV', value: 'IPBV' },
         { label: 'Outra', value: 'Outra' },
       ],
     },
     {
+      label: 'Nascimento',
+      name: 'personalInformation.birthday',
+      type: 'text',
+      placeholder: '01/01/1990',
+      oddOrEven: 'odd',
+      required: true,
+      errorMessage: 'Insira uma data de nascimento válida no formato (00/00/0000)',
+    },
+    {
+      label: 'CPF',
+      name: 'personalInformation.cpf',
+      type: 'number',
+      placeholder: '1234567890',
+      oddOrEven: 'even',
+      required: true,
+      errorMessage: 'Insira um CPF válido',
+    },
+    { label: 'RG', name: 'personalInformation.rg', type: 'number', placeholder: '0123456', oddOrEven: 'odd' },
+    {
+      label: 'Órgão Expedidor',
+      name: 'personalInformation.rgShipper',
+      type: 'text',
+      placeholder: 'SDS',
+      oddOrEven: 'even',
+    },
+    {
+      label: 'Estado Expedidor',
+      name: 'personalInformation.rgShipperState',
+      type: 'text',
+      placeholder: 'PE',
+      oddOrEven: 'odd',
+    },
+    {
       label: 'Vai de Carro',
       name: 'contact.car',
       type: 'select',
       placeholder: 'Selecione se vai de carro',
+      oddOrEven: 'even',
       options: [
-        { label: 'Sim', value: 'Sim' },
-        { label: 'Não', value: 'Não' },
+        { label: 'Sim', value: true },
+        { label: 'Não', value: false },
       ],
     },
     {
@@ -61,55 +109,64 @@ const AdminTableColumns = ({ addFormData, editFormData, handleFormChange, addFor
       name: 'contact.needRide',
       type: 'select',
       placeholder: 'Selecione se precisa de carona',
+      oddOrEven: 'odd',
       options: [
-        { label: 'Sim', value: 'Sim' },
-        { label: 'Não', value: 'Não' },
+        { label: 'Sim', value: true },
+        { label: 'Não', value: false },
       ],
     },
-    { label: 'Vagas de Carona', name: 'contact.numberVacancies', type: 'number', placeholder: '0' },
+    { label: 'Vagas de Carona', name: 'contact.numberVacancies', type: 'number', placeholder: '0', oddOrEven: 'even' },
     {
-      label: 'Data de Inscrição',
-      name: 'registrationDate',
+      label: 'Observação de Carona',
+      name: 'contact.rideObservation',
       type: 'text',
-      value: editForm ? editFormData.registrationDate : formattedDate,
-      disabled: true,
+      placeholder: 'Observação de carona',
+      oddOrEven: 'odd',
     },
-    { label: 'Nascimento', name: 'personalInformation.birthday', type: 'text', placeholder: '01/01/1990' },
-    { label: 'CPF', name: 'personalInformation.cpf', type: 'number', placeholder: '1234567890' },
-    { label: 'RG', name: 'personalInformation.rg', type: 'number', placeholder: '0123456' },
-    { label: 'Órgão Expedidor', name: 'personalInformation.rgShipper', type: 'text', placeholder: 'SDS' },
-    { label: 'Estado Expedidor', name: 'personalInformation.rgShipperState', type: 'text', placeholder: 'PE' },
     {
       label: 'Categoria',
       name: 'personalInformation.gender',
       type: 'select',
       placeholder: 'Selecione sua categoria/gênero',
+      oddOrEven: 'even',
       options: [
         { label: 'Criança', value: 'Criança' },
         { label: 'Homem', value: 'Homem' },
         { label: 'Mulher', value: 'Mulher' },
       ],
     },
-    { label: 'Celular', name: 'contact.cellPhone', type: 'number', placeholder: '81993727854' },
+    {
+      label: 'Data de Inscrição',
+      name: 'registrationDate',
+      type: 'text',
+      value: editForm ? editFormData.registrationDate : currentDate,
+      disabled: true,
+      oddOrEven: 'odd',
+    },
+    { label: 'Celular', name: 'contact.cellPhone', type: 'number', placeholder: '81993727854', oddOrEven: 'even' },
     {
       label: 'Whatsapp',
       name: 'contact.isWhatsApp',
       type: 'select',
       placeholder: 'Selecione se número é whatsapp',
+      oddOrEven: 'odd',
       options: [
-        { label: 'Sim', value: 'Sim' },
-        { label: 'Não', value: 'Não' },
+        { label: 'Sim', value: true },
+        { label: 'Não', value: false },
       ],
     },
-    { label: 'Email', name: 'contact.email', type: 'text', placeholder: 'teste@teste.com' },
-    { label: 'Preço Total', name: 'totalPrice', type: 'number', placeholder: '500' },
-    { label: 'Alergia', name: 'contact.allergy', type: 'text', placeholder: 'Quais alergias' },
-    { label: 'Agregados', name: 'contact.aggregate', type: 'text', placeholder: 'Quais agregados' },
+    { label: 'Email', name: 'contact.email', type: 'text', placeholder: 'teste@teste.com', oddOrEven: 'even' },
+    { label: 'Preço', name: 'totalPrice', type: 'number', placeholder: '500', oddOrEven: 'odd' },
+    { label: 'Cupom', name: 'package.discountCoupon', type: 'text', placeholder: 'NOMEDAPESSOA100', oddOrEven: 'even' },
+    { label: 'Alergia', name: 'contact.allergy', type: 'text', placeholder: 'Quais alergias', oddOrEven: 'odd' },
+    { label: 'Agregados', name: 'contact.aggregate', type: 'text', placeholder: 'Quais agregados', oddOrEven: 'even' },
+    // ==================================================
     {
       label: 'Acomodação',
       name: 'package.accomodation.name',
       type: 'select',
       placeholder: 'Selecione a acomodação',
+      oddOrEven: 'odd',
       options: [
         { label: 'Colégio', value: 'Colégio' },
         { label: 'Seminário', value: 'Seminário' },
@@ -121,6 +178,7 @@ const AdminTableColumns = ({ addFormData, editFormData, handleFormChange, addFor
       name: 'package.accomodation.subAccomodation',
       type: 'select',
       placeholder: 'Selecione a sub acomodação',
+      oddOrEven: 'even',
       options: [
         { label: 'Colégio Individual', value: 'Colégio Individual' },
         { label: 'Colégio Família', value: 'Colégio Família' },
@@ -129,39 +187,77 @@ const AdminTableColumns = ({ addFormData, editFormData, handleFormChange, addFor
         { label: 'Outra', value: 'Outra' },
       ],
     },
+    // ==================================================
     {
-      label: 'Alimentação Extra',
-      name: 'extraMeals.someFood',
+      label: 'Transporte',
+      name: 'package.transportation',
       type: 'select',
-      placeholder: 'Selecione se terá alimentação extra',
+      placeholder: 'Selecione tipo de transporte',
+      oddOrEven: 'odd',
       options: [
-        { label: 'Sim', value: 'Sim' },
-        { label: 'Não', value: 'Não' },
+        { label: 'Com Ônibus', value: 'Com Ônibus' },
+        { label: 'Sem Ônibus', value: 'Sem Ônibus' },
       ],
-    },
-    {
-      label: 'Dias de Alimentação Extra',
-      name: 'extraMeals.extraMeals',
-      type: 'text',
-      placeholder: 'Domingo, Segunda, Terça, etc.',
     },
     {
       label: 'Alimentação',
       name: 'package.food',
       type: 'text',
       placeholder: 'Café da manhã, almoço e jantar; Almoço e jantar ou Sem Alimentação',
+      oddOrEven: 'even',
     },
     {
-      label: 'Transporte',
-      name: 'package.transportation',
+      label: 'Alimentação Extra',
+      name: 'extraMeals.someFood',
       type: 'select',
-      placeholder: 'Selecione tipo de transporte',
+      placeholder: 'Selecione se terá alimentação extra',
+      oddOrEven: 'odd',
       options: [
-        { label: 'Com Ônibus', value: 'Com Ônibus' },
-        { label: 'Sem Ônibus', value: 'Sem Ônibus' },
+        { label: 'Sim', value: true },
+        { label: 'Não', value: false },
       ],
     },
+    {
+      label: 'Alimentação Extra (dias)',
+      name: 'extraMeals.extraMeals',
+      type: 'text',
+      placeholder: 'Domingo, Segunda, Terça, etc.',
+      oddOrEven: 'even',
+    },
+    {
+      label: 'Observação',
+      name: 'observation',
+      type: 'text',
+      placeholder: 'Observação sobre essa inscrição',
+      oddOrEven: 'odd',
+    },
   ];
+
+  const validateFields = () => {
+    const missing = [];
+
+    fields.forEach((field) => {
+      if (field.required) {
+        const fieldValue = editForm
+          ? editFormData[field.name.split('.')[0]]?.[field.name.split('.')[1]]
+          : addFormData[field.name.split('.')[0]]?.[field.name.split('.')[1]];
+
+        if (!fieldValue) {
+          missing.push(field.name);
+        }
+      }
+    });
+
+    return missing;
+  };
+
+  useEffect(() => {
+    const missing = validateFields();
+
+    if (JSON.stringify(missing) !== JSON.stringify(missingFields)) {
+      setMissingFields(missing);
+    }
+  }, [addFormData, editFormData]);
 
   return (
     <Row>
@@ -175,16 +271,21 @@ const AdminTableColumns = ({ addFormData, editFormData, handleFormChange, addFor
             field.name === 'registrationDate'
               ? editForm
                 ? editFormData.registrationDate
-                : formattedDate
+                : currentDate
               : editForm
-              ? editFormData[field.name.split('.')[0]][field.name.split('.')[1]]
+              ? editFormData[field.name.split('.')[0]]?.[field.name.split('.')[1]]
               : addFormData[field.name.split('.')[0]]?.[field.name.split('.')[1]]
           }
           onChange={handleFormChange}
-          placeholder={addForm ? field.placeholder : ''}
+          placeholder={addForm ? (field.placeholder ? field.placeholder : 'Selecione uma opção') : ''}
           addForm={addForm}
           disabled={field.disabled || false}
           options={field.options || []}
+          required={field.required}
+          errorMessage={field.errorMessage}
+          oddOrEven={field.oddOrEven}
+          formSubmitted={formSubmitted}
+          missingFields={missingFields}
         />
       ))}
     </Row>
