c8715ff65b547f662b4e80fc01995615f7828784
Álvaro Leal <alvaro.leal@liferay.com>
2024-08-21 07:28:10 -0300

    Update enpoint url

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export const JWT_LOCAL_STORAGE_KEY = 'token_jwt';
 
-export const BASE_URL = 'http://ec2-35-89-80-98.us-west-2.compute.amazonaws.com:8080';
+export const BASE_URL = 'https://wilsondev.com.br';
 
 export const LOGIN_ROUTE = '/admin';
 
