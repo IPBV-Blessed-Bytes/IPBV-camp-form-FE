a5cd156850061368c5edc2aa3a4ad13bc021780a
Álvaro Leal <alvaro.leal@liferay.com>
2024-09-13 23:15:23 -0300

    Update admin loggedIn component to make requests to fill admin cards

--- src/Pages/Admin/admin.jsx
+++ src/Pages/Admin/admin.jsx
@@ -9,140 +9,20 @@ import useAuth from '../../hooks/useAuth';
 
 const AdminHome = ({ totalRegistrationsGlobal }) => {
   const isAdminPathname = window.location.pathname === '/admin';
-  const [availablePackages, setAvailablePackages] = useState({});
   const [showPassword, setShowPassword] = useState(false);
-
   const navigateTo = useNavigate();
   const { login, logout, user, isLoggedIn } = useAuth();
-  const [loginData, setLoginData] = useState({
-    username: '',
-    password: '',
-  });
+  const [loginData, setLoginData] = useState({ username: '', password: '' });
 
-  const scrollUp = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-  };
+  const scrollUp = () => window.scrollTo({ top: 0, behavior: 'smooth' });
 
   const handleLogin = () => {
     login(loginData.login, loginData.password);
-
     scrollUp();
-
-    setLoginData((prevLoginData) => ({
-      ...prevLoginData,
-
-      password: '',
-    }));
+    setLoginData((prevLoginData) => ({ ...prevLoginData, password: '' }));
   };
 
-  const handleKeyDown = (e) => {
-    if (e.key === 'Enter') {
-      handleLogin();
-    }
-  };
-
-  const totalRegistrations = totalRegistrationsGlobal.totalRegistrations;
-  const totalValidRegistrationsPaied = totalRegistrationsGlobal.totalValidRegistrationsPaied;
-  const totalValidRegistrationsGlobal = totalRegistrationsGlobal.totalValidRegistrationsGlobal;
-  const totalChildren = totalRegistrationsGlobal.totalChildren;
-  const totalNonPaied = totalRegistrationsGlobal.totalNonPaied;
-  const availablePackagesTotal = availablePackages.totalPackages || {};
-  const availablePackagesUsed = availablePackages.usedPackages || {};
-
-  const schoolIndividualTotal = availablePackagesTotal?.colegioIndividual || 0;
-  const schoolFamilyTotal = availablePackagesTotal?.colegioFamilia || 0;
-  const schoolCampingTotal = availablePackagesTotal?.colegioCamping || 0;
-  const seminaryTotal = availablePackagesTotal?.seminario || 0;
-  const otherTotal = availablePackagesTotal?.outro || 0;
-
-  const schoolIndividualWithBus = availablePackagesUsed.colegioIndividualComOnibus;
-  const schoolFamilyWithBus = availablePackagesUsed.colegioFamiliaComOnibus;
-  const seminaryWithBus = availablePackagesUsed.seminarioIndividualComOnibus;
-  const otherWithBus = availablePackagesUsed.outroComOnibus;
-
-  const totalVacanciesWithBuses = schoolIndividualWithBus + schoolFamilyWithBus + seminaryWithBus + otherWithBus;
-
-  const calculateVacancies = (usedPackages, usedValidPackages, totalPackages, withBus, withoutBus, specificTotals) => {
-    if (
-      usedPackages &&
-      usedValidPackages &&
-      totalPackages &&
-      Object.keys(usedPackages).length > 0 &&
-      Object.keys(usedValidPackages).length > 0 &&
-      Object.keys(totalPackages).length > 0
-    ) {
-      const filledVacancies = usedValidPackages[withBus] + usedValidPackages[withoutBus];
-      const totalVacancies = specificTotals[withBus];
-
-      const remainingVacancies = totalVacancies - filledVacancies;
-      return { filledVacancies, remainingVacancies };
-    } else {
-      return { filledVacancies: '0', remainingVacancies: '0' };
-    }
-  };
-
-  const createCardData = (title, withBusKey, withoutBusKey, background, titleColor) => {
-    const usedPackages = availablePackages.usedPackages || {};
-    const totalPackages = availablePackages.totalPackages || {};
-    const usedValidPackages = availablePackages.usedValidPackages || {};
-
-    const { filledVacancies, remainingVacancies } = calculateVacancies(
-      usedPackages,
-      usedValidPackages,
-      totalPackages,
-      withBusKey,
-      withoutBusKey,
-      {
-        colegioIndividualComOnibus: schoolIndividualTotal,
-        colegioFamiliaComOnibus: schoolFamilyTotal,
-        colegioCampingSemAlimentacao: schoolCampingTotal,
-        seminarioIndividualComOnibus: seminaryTotal,
-        outroComOnibus: otherTotal,
-      },
-    );
-
-    return {
-      title,
-      remainingVacancies,
-      filledVacancies,
-      background,
-      titleColor,
-    };
-  };
-
-  const cards = [
-    createCardData(
-      'Colégio XV - Individual',
-      'colegioIndividualComOnibus',
-      'colegioIndividualSemOnibus',
-      'bg-light',
-      'text-success',
-    ),
-    createCardData(
-      'Colégio XV - Família',
-      'colegioFamiliaComOnibus',
-      'colegioFamiliaSemOnibus',
-      'bg-light',
-      'text-success',
-    ),
-    createCardData(
-      'Colégio XV - Camping',
-      'colegioCampingSemAlimentacao',
-      'colegioCampingComAlimentacao',
-      'bg-light',
-      'text-success',
-    ),
-    createCardData(
-      'Seminário São José',
-      'seminarioIndividualComOnibus',
-      'seminarioIndividualSemOnibus',
-      'bg-light',
-      'text-success',
-    ),
-    createCardData('Outra Acomodação', 'outroComOnibus', 'outroSemOnibusSemAlimentacao', 'bg-light', 'text-success'),
-  ];
-
-  const firstRowCards = cards.slice(0, 6);
+  const handleKeyDown = (e) => e.key === 'Enter' && handleLogin();
 
   const handleShowPassword = () => {
     setShowPassword(!showPassword);
@@ -167,13 +47,7 @@ const AdminHome = ({ totalRegistrationsGlobal }) => {
             <AdminLoggedIn
               loggedInUsername={user ? user : 'Usuário não identificado'}
               handleLogout={logout}
-              firstRowCards={firstRowCards}
-              totalValidRegistrationsPaied={totalValidRegistrationsPaied}
-              totalValidRegistrationsGlobal={totalValidRegistrationsGlobal}
-              totalRegistrations={totalRegistrations}
-              totalChildren={totalChildren}
-              totalNonPaied={totalNonPaied}
-              totalVacanciesWithBuses={totalVacanciesWithBuses}
+              totalRegistrationsGlobal={totalRegistrationsGlobal}
             />
           )}
         </Container>
@@ -183,7 +57,7 @@ const AdminHome = ({ totalRegistrationsGlobal }) => {
 };
 
 AdminHome.propTypes = {
-  totalRegistrationsGlobal: PropTypes.func,
+  totalRegistrationsGlobal: PropTypes.object.isRequired,
 };
 
 export default AdminHome;
--- src/Pages/Admin/adminComponents/adminLoggedIn.jsx
+++ src/Pages/Admin/adminComponents/adminLoggedIn.jsx
@@ -10,17 +10,7 @@ import { BASE_URL } from '../../../config/index';
 import Loading from '../../../components/Loading';
 import Icons from '../../../components/Icons';
 
-const AdminLoggedIn = ({
-  loggedInUsername,
-  handleLogout,
-  firstRowCards,
-  totalValidRegistrationsPaied,
-  totalValidRegistrationsGlobal,
-  totalRegistrations,
-  totalChildren,
-  totalNonPaied,
-  totalVacanciesWithBuses,
-}) => {
+const AdminLoggedIn = ({ loggedInUsername, handleLogout, totalRegistrationsGlobal }) => {
   const [loading, setLoading] = useState(true);
   const [availablePackages, setAvailablePackages] = useState(true);
   const navigate = useNavigate();
@@ -52,6 +42,89 @@ const AdminLoggedIn = ({
 
     fetchPackages();
   }, []);
+
+  const totalRegistrations = totalRegistrationsGlobal.totalRegistrations;
+  const totalValidRegistrations = totalRegistrationsGlobal.totalValidRegistrations;
+  const totalChildren = totalRegistrationsGlobal.totalChildren;
+  const availablePackagesTotal = availablePackages.totalPackages || {};
+  const availablePackagesUsed = availablePackages.usedPackages || {};
+  const totalVacanciesWithBuses =
+    availablePackagesUsed.colegioFamiliaComOnibusComAlimentacao +
+      availablePackagesUsed.colegioFamiliaComOnibusSemAlimentacao +
+      availablePackagesUsed.colegioIndividualComOnibusComAlimentacao +
+      availablePackagesUsed.colegioIndividualComOnibusSemAlimentacao +
+      availablePackagesUsed.outroComOnibusComAlimentacao +
+      availablePackagesUsed.seminarioIndividualComOnibusComAlimentacao || {};
+
+  const packageCardsData = [
+    {
+      title: 'Colégio Individual',
+      remainingVacancies: availablePackagesTotal?.colegioIndividual || '0',
+      filledVacancies:
+        availablePackagesUsed?.colegioIndividualComOnibusComAlimentacao +
+          availablePackagesUsed?.colegioIndividualComOnibusSemAlimentacao +
+          availablePackagesUsed?.colegioIndividualSemOnibusComAlimentacao +
+          availablePackagesUsed?.colegioIndividualSemOnibusSemAlimentacao || '0',
+    },
+    {
+      title: 'Colégio Família',
+      remainingVacancies: availablePackagesTotal?.colegioFamilia || '0',
+      filledVacancies:
+        availablePackagesUsed?.colegioFamiliaComOnibusComAlimentacao +
+          availablePackagesUsed?.colegioFamiliaComOnibusSemAlimentacao +
+          availablePackagesUsed?.colegioFamiliaSemOnibusComAlimentacao +
+          availablePackagesUsed?.colegioFamiliaSemOnibusSemAlimentacao || '0',
+    },
+    {
+      title: 'Colégio Camping',
+      remainingVacancies: availablePackagesTotal?.colegioCamping || '0',
+      filledVacancies:
+        availablePackagesUsed?.colegioCampingSemOnibusComAlimentacao +
+          availablePackagesUsed?.colegioCampingSemOnibusSemAlimentacao || '0',
+    },
+    {
+      title: 'Seminário',
+      remainingVacancies: availablePackagesTotal?.seminario || '0',
+      filledVacancies:
+        availablePackagesUsed?.seminarioIndividualComOnibusComAlimentacao +
+          availablePackagesUsed?.seminarioIndividualSemOnibusComAlimentacao || '0',
+    },
+    {
+      title: 'Outra Acomodação',
+      remainingVacancies: availablePackagesTotal?.outro || '0',
+      filledVacancies:
+        availablePackagesUsed?.outroComOnibusComAlimentacao + availablePackagesUsed?.outroSemOnibusSemAlimentacao ||
+        '0',
+    },
+    {
+      title: 'Usuário Sem Custo',
+      remainingVacancies: availablePackagesTotal?.usuarioSemCusto || '0',
+      filledVacancies: availablePackagesUsed?.usuarioSemCusto || '0',
+    },
+  ];
+
+  const totalCardsData = [
+    {
+      title: 'Total de Inscritos Geral',
+      remainingVacancies: 600 - totalRegistrations || '0',
+      filledVacancies: totalRegistrations || '0',
+    },
+    {
+      title: 'Total de Inscritos Válidos',
+      remainingVacancies: 600 - totalValidRegistrations || '0',
+      filledVacancies: totalValidRegistrations || '0',
+    },
+    {
+      title: 'Total de Crianças',
+      remainingVacancies: 'Ilimitado',
+      filledVacancies: totalChildren || '0',
+    },
+    {
+      title: 'Total de Inscritos com Ônibus',
+      remainingVacancies: 98 - totalVacanciesWithBuses || '0',
+      filledVacancies: totalVacanciesWithBuses || '0',
+    },
+  ];
 
   return (
     <>
@@ -125,114 +198,24 @@ const AdminLoggedIn = ({
           </Button>
         </Col>
       </Row>
-      <Row>
-        <h4 className="text-center fw-bold mb-4">PACOTES:</h4>
-        {firstRowCards.map((card) => (
-          <AdminPackageCard key={card.title} {...card} />
-        ))}
-      </Row>
 
-      <Row className="mt-4">
-        <h4 className="text-center fw-bold mb-4">TOTAL:</h4>
-      </Row>
+      {!loading && (
+        <>
+          <Row>
+            <h4 className="text-center fw-bold mb-4">PACOTES:</h4>
+            {packageCardsData.map((card) => (
+              <AdminPackageCard key={card.title} {...card} cardType="package-card" />
+            ))}
+          </Row>
 
-      <Row>
-        <Col className="mb-4" xs={12} md={6} lg={4}>
-          <Card className="h-100 bg-dark">
-            <Card.Body>
-              <Card.Title className="fw-bold text-warning">Total de Inscritos válidos (pagantes)</Card.Title>
-              <Card.Text>
-                Vagas Totais Preenchidas:{' '}
-                <em>
-                  <b>{isNaN(totalValidRegistrationsPaied) ? 'Indefinido' : totalValidRegistrationsPaied.toString()}</b>
-                </em>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col className="mb-4" xs={12} md={6} lg={4}>
-          <Card className="h-100 bg-dark">
-            <Card.Body>
-              <Card.Title className="fw-bold text-warning">
-                Total de Inscritos válidos <br />
-                (pagantes e não pagantes)
-              </Card.Title>
-              <Card.Text>
-                Vagas Totais Preenchidas:{' '}
-                <em>
-                  <b>
-                    {isNaN(totalValidRegistrationsGlobal) ? 'Indefinido' : totalValidRegistrationsGlobal.toString()}
-                  </b>
-                </em>
-                <br />
-                Vagas Totais Restantes:{' '}
-                <em>
-                  <b>
-                    {isNaN(375 - totalValidRegistrationsGlobal)
-                      ? 'Indefinido'
-                      : (375 - totalValidRegistrationsGlobal).toString()}
-                  </b>
-                </em>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col className="mb-4" xs={12} md={6} lg={4}>
-          <Card className="h-100 bg-dark">
-            <Card.Body>
-              <Card.Title className="fw-bold text-warning">
-                Total de Inscritos geral <br />
-                (adultos e crianças)
-              </Card.Title>
-              <Card.Text>
-                Vagas Totais Preenchidas:{' '}
-                <em>
-                  <b>{isNaN(totalRegistrations) ? 'Indefinido' : totalRegistrations.toString()}</b>
-                </em>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col className="mb-4" xs={12} md={6} lg={4}>
-          <Card className="h-100 bg-dark">
-            <Card.Body>
-              <Card.Title className="fw-bold text-warning">Total de Crianças</Card.Title>
-              <Card.Text>
-                Vagas Totais Preenchidas:{' '}
-                <em>
-                  <b>{isNaN(totalChildren) ? 'Indefinido' : totalChildren.toString()}</b>
-                </em>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col className="mb-4" xs={12} md={6} lg={4}>
-          <Card className="h-100 bg-dark">
-            <Card.Body>
-              <Card.Title className="fw-bold text-warning">Total de Não Pagantes</Card.Title>
-              <Card.Text>
-                Vagas Totais Preenchidas:{' '}
-                <em>
-                  <b>{isNaN(totalNonPaied) ? 'Indefinido' : totalNonPaied.toString()}</b>
-                </em>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-        <Col className="mb-4" xs={12} md={6} lg={4}>
-          <Card className="h-100 bg-warning">
-            <Card.Body>
-              <Card.Title className="fw-bold text-dark">Ônibus Preenchidos</Card.Title>
-              <Card.Text>
-                Vagas Preenchidas com Ônibus:{' '}
-                <em>
-                  <b>{isNaN(totalVacanciesWithBuses) ? 'Indefinido' : totalVacanciesWithBuses.toString()}</b>
-                </em>
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </Col>
-      </Row>
+          <Row className="mt-4">
+            <h4 className="text-center fw-bold mb-4">TOTAL:</h4>
+            {totalCardsData.map((card) => (
+              <AdminPackageCard key={card.title} {...card} cardType="total-card" />
+            ))}
+          </Row>
+        </>
+      )}
 
       <div className="packages-horizontal-line" />
       <h4>Notas:</h4>
@@ -240,28 +223,27 @@ const AdminLoggedIn = ({
         <ul>
           <li>
             <em>
-              <b>Inscritos Válidos</b>
-            </em>{' '}
-            : Contagem de adultos que contam como uma inscrição válida
+              <b>Total de Inscritos Geral</b>
+            </em>
+            : Contagem de adultos e crianças
           </li>
           <li>
             <em>
-              <b>Não Pagantes</b>
-            </em>{' '}
-            : Pessoas que não irão dormir, comer ou se transportar no ônibus, apenas assistir aos cultos e/ou participar
-            das programações
+              <b>Total de Inscritos Válidos</b>
+            </em>
+            : Contagem de adultos pagantes
           </li>
           <li>
             <em>
-              <b>Total de inscritos geral</b>
-            </em>{' '}
-            : Contagem de inscritos válidos (pagantes ou não) e crianças
+              <b>Total de Crianças</b>
+            </em>
+            : Contagem de crianças
           </li>
           <li>
             <em>
-              <b>Ônibus Preenchidos</b>
-            </em>{' '}
-            : Contagem de pessoas inscritas nos ônibus
+              <b>Total de Inscritos Com Ônibus</b>
+            </em>
+            : Contagem de pessoas válidas que irão de ônibus
           </li>
         </ul>
       </div>
@@ -276,15 +258,14 @@ const AdminLoggedIn = ({
 };
 
 AdminLoggedIn.propTypes = {
-  loggedInUsername: PropTypes.bool,
-  handleLogout: PropTypes.func,
-  firstRowCards: PropTypes.bool,
-  totalValidRegistrationsPaied: PropTypes.bool,
-  totalValidRegistrationsGlobal: PropTypes.bool,
-  totalRegistrations: PropTypes.bool,
-  totalChildren: PropTypes.bool,
-  totalNonPaied: PropTypes.bool,
-  totalVacanciesWithBuses: PropTypes.bool,
+  loggedInUsername: PropTypes.string.isRequired,
+  handleLogout: PropTypes.func.isRequired,
+  totalRegistrationsGlobal: PropTypes.shape({
+    totalRegistrations: PropTypes.number,
+    totalChildren: PropTypes.number,
+    totalFilledVacancies: PropTypes.number,
+    totalValidRegistrations: PropTypes.number,
+  }).isRequired,
 };
 
 export default AdminLoggedIn;
