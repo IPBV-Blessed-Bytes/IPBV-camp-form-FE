174d6aa24369fc2db012d7617ab5d39109c7a284
Álvaro Leal <alvaro.leal@liferay.com>
2024-10-01 05:53:12 -0300

    Add aggregate permission

--- src/fetchers/permissions.js
+++ src/fetchers/permissions.js
@@ -9,10 +9,11 @@ export const permissions = (userRole, context) => {
     'registered-button-home': userRole === ADMIN || userRole === COLLABORATOR || userRole === COLLABORATOR_VIEWER,
     'ride-button-home': userRole === ADMIN || userRole === COLLABORATOR,
     'coupon-button-home': userRole === ADMIN || userRole === COLLABORATOR || userRole === COLLABORATOR_VIEWER,
+    'aggregate-button-home': userRole === ADMIN || userRole === COLLABORATOR,
     'packages-and-totals-cards-home': userRole === ADMIN || userRole === COLLABORATOR || userRole === COLLABORATOR_VIEWER,
     'utilities-links-home': userRole === ADMIN || userRole === COLLABORATOR || userRole === COLLABORATOR_VIEWER,
     'advanced-options-admin-table': userRole === ADMIN || userRole === COLLABORATOR,
-    'checkin': userRole === ADMIN || userRole === COLLABORATOR || userRole === CHECKER,
+    'checkin': userRole === ADMIN || userRole === CHECKER,
   };
 
   return permissionsMap[context] || false;
