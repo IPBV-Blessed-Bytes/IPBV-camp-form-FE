d4af8525d06fdb85dcdc4cb72155863a35827c3b
Álvaro Leal <alvaro.leal@liferay.com>
2024-10-17 07:09:24 -0300

    Add table component inside admin feedback

--- src/Pages/Admin/adminComponents/adminFeedback.jsx
+++ src/Pages/Admin/adminComponents/adminFeedback.jsx
@@ -31,7 +31,24 @@ const AdminFeedback = () => {
       </Row>
       <hr className="horizontal-line" />
 
-      <Row></Row>
+      <Row>
+        <Table striped bordered hover responsive className="custom-table">
+          <thead>
+            <tr>
+              <th className="table-cells-header">Nome:</th>
+              <th className="table-cells-header">Organização:</th>
+              <th className="table-cells-header">Experiência:</th>
+              <th className="table-cells-header">Alimentação:</th>
+              <th className="table-cells-header">Programação:</th>
+              <th className="table-cells-header">Estrutura Física:</th>
+              <th className="table-cells-header">Acolhimento:</th>
+              <th className="table-cells-header">Probabilidade de Volta:</th>
+              <th className="table-cells-header">Campo Aberto:</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </Table>
+      </Row>
 
       <Loading loading={loading} />
     </Container>
