a2e1d389f3844bb6b2b09f6e05b3414c4c2c5159
Álvaro Leal <alvaro.leal@liferay.com>
2024-10-27 14:34:14 -0300

    Remove handle logout to admin logget out context

--- src/Pages/Admin/AdminPages/adminLoggedOut.jsx
+++ src/Pages/Admin/AdminPages/adminLoggedOut.jsx
@@ -21,7 +21,6 @@ const AdminLoggedOut = ({
           className="fw-bold"
           variant="danger"
           onClick={() => {
-            handleLogout();
             navigateTo('/');
           }}
         >
