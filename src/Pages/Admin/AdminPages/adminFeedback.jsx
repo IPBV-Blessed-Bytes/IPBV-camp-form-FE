b11a08ec771db2e44703878331b4e2431d8d3921
Álvaro Leal <alvaro.leal@liferay.com>
2024-10-30 23:15:55 -0300

    Add admin feedback component

--- src/Pages/Admin/AdminPages/adminFeedback.jsx
+++ src/Pages/Admin/AdminPages/adminFeedback.jsx
@@ -1,38 +1,68 @@
 import { useState, useEffect } from 'react';
-import { Container, Row, Table } from 'react-bootstrap';
+import { Container, Table } from 'react-bootstrap';
 import fetcher from '@/fetchers/fetcherWithCredentials';
 import Loading from '@/components/Loading';
 import AdminHeader from '../AdminComponents/adminHeader';
 import scrollUp from '@/hooks/useScrollUp';
 
+const TABLE_HEADERS = [
+  'ID',
+  'Nome',
+  'Organização',
+  'Experiência',
+  'Alimentação',
+  'Programação',
+  'Estrutura Física',
+  'Acolhimento',
+  'Probabilidade de Volta',
+  'Campo Aberto',
+];
+
 const AdminFeedback = () => {
-  const [loading, setLoading] = useState(false);
+  const [feedbacks, setFeedbacks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   scrollUp();
 
+  console.log(feedbacks);
+
+  useEffect(() => {
+    const fetchFeedbacks = async () => {
+      try {
+        const { data } = await fetcher.get('feedback');
+        setFeedbacks(data);
+      } catch (error) {
+        console.error('Erro ao carregar feedbacks:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchFeedbacks();
+  }, []);
+
   return (
     <Container fluid>
-      <AdminHeader pageName="Gerenciamento de Feedbacks" sessionTypeIcon="feedback" iconSize={80} fill={'#204691'} />
-
-      <Row>
-        <Table striped bordered hover responsive className="custom-table">
-          <thead>
-            <tr>
-              <th className="table-cells-header">Nome:</th>
-              <th className="table-cells-header">Organização:</th>
-              <th className="table-cells-header">Experiência:</th>
-              <th className="table-cells-header">Alimentação:</th>
-              <th className="table-cells-header">Programação:</th>
-              <th className="table-cells-header">Estrutura Física:</th>
-              <th className="table-cells-header">Acolhimento:</th>
-              <th className="table-cells-header">Probabilidade de Volta:</th>
-              <th className="table-cells-header">Campo Aberto:</th>
+      <AdminHeader pageName="Gerenciamento de Feedbacks" sessionTypeIcon="feedback" iconSize={80} fill="#204691" />
+      <Table striped bordered hover responsive className="custom-table mt-3">
+        <thead>
+          <tr>
+            {TABLE_HEADERS.map((header, index) => (
+              <th key={index} className="table-cells-header">
+                {header}:
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {feedbacks.map((feedback, index) => (
+            <tr key={index}>
+              {Object.values(feedback).map((value, index) => (
+                <td key={index}>{value}</td>
+              ))}
             </tr>
-          </thead>
-          <tbody></tbody>
-        </Table>
-      </Row>
-
+          ))}
+        </tbody>
+      </Table>
       <Loading loading={loading} />
     </Container>
   );
