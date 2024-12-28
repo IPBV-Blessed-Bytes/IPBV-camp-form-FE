33218f08c3ef3840559bfdda1604e868bf964760
Álvaro Leal <alvaro.leal@liferay.com>
2024-12-28 18:41:53 -0300

    Update logic to filter values in the admin table

--- src/components/Admin/TableColumnFilter.jsx
+++ src/components/Admin/TableColumnFilter.jsx
@@ -1,17 +1,29 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import PropTypes from 'prop-types';
+import { useEffect } from 'react';
 import Icons from '@/components/Global/Icons';
 
-const TableColumnFilter = ({ column }) => {
+const TableColumnFilter = ({ column, onFilterChange }) => {
   const filterValue = column?.filterValue || '';
   const setFilter = column?.setFilter || (() => {});
+
+  useEffect(() => {
+    onFilterChange();
+  }, [filterValue]);
+
+  const handleFilterChange = (e) => {
+    const value = e.target.value || undefined;
+    setFilter(value);
+  };
 
   return (
     <div className="d-flex position-relative">
       <input
         className={`filter-input ${filterValue && 'actived-filter'}`}
         value={filterValue}
-        onChange={(e) => setFilter(e.target.value || undefined)}
+        onChange={(e) => {
+          handleFilterChange(e);
+        }}
         placeholder="Pesquise ..."
       />
       {filterValue && <Icons className="filter-icon" typeIcon="filter" iconSize={24} fill="#4267a7" />}
@@ -21,9 +33,10 @@ const TableColumnFilter = ({ column }) => {
 
 TableColumnFilter.propTypes = {
   column: PropTypes.shape({
-    filterValue: PropTypes.bool,
+    filterValue: PropTypes.string,
     setFilter: PropTypes.func,
   }),
+  onFilterChange: PropTypes.func,
 };
 
 export default TableColumnFilter;
