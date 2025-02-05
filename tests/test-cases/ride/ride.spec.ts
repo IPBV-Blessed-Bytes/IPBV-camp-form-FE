0aa4860669fa33431b24b23370e63a092ded15a6
Álvaro Leal <alvaro.leal@liferay.com>
2025-02-05 10:51:01 -0300

    Fix ride test describe

--- tests/test-cases/ride/ride.spec.ts
+++ tests/test-cases/ride/ride.spec.ts
@@ -2,5 +2,5 @@ import { expect } from '@playwright/test';
 import { rideTest as test } from 'tests/fixtures/rideTest';
 import { testsConfig } from 'tests/tests.config';
 
-test.describe('Camper table flow', () => {
+test.describe('Ride flow', () => {
 });
