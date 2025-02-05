f874dc959239fe0c0c8cb5ea712a0588c9e7bb01
Álvaro Leal <alvaro.leal@liferay.com>
2025-02-05 10:16:10 -0300

    Add initial scope of automated tests (common tests - bypass context)

--- tests/fixtures/commonTest.ts
+++ tests/fixtures/commonTest.ts
@@ -1,12 +1,17 @@
 import { test } from '@playwright/test';
 import { PermissionsComponent } from 'tests/pages/PermissionPage';
+import { BypassComponent } from 'tests/pages/BypassPage';
 
 interface CommonTest {
   permission: PermissionsComponent;
+  bypass: BypassComponent;
 }
 
 export const commonTest = test.extend<CommonTest>({
   permission: async ({ page }, use) => {
     await use(new PermissionsComponent(page));
+  },
+  bypass: async ({ page }, use) => {
+    await use(new BypassComponent(page));
   },
 });
