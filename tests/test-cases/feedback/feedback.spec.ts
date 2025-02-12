0380646009e0293adde17ab0bfb780d5ea009762
Álvaro Leal <alvaro.leal@liferay.com>
2025-02-12 11:25:48 -0300

    Remove feedback tests

--- tests/test-cases/feedback/feedback.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect } from '@playwright/test';
-import { feedbackTest as test } from 'tests/fixtures/feedbackTest';
-import { testsConfig } from 'tests/tests.config';
-
-test.describe('Feedback flow', () => {
-});
