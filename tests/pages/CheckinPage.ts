531869783be8f4d63b64355d701704e02a7e2e47
Álvaro Leal <alvaro.leal@liferay.com>
2025-02-12 18:50:46 -0300

    Add checkin tests

--- tests/pages/CheckinPage.ts
+++ tests/pages/CheckinPage.ts
@@ -1,8 +1,88 @@
 import { Locator, Page } from '@playwright/test';
 
 export class CheckinComponent {
+  readonly registeredButton: Locator;
+  readonly registeredHeading: Locator;
+  readonly checkinFieldInTheCampersTable: Locator;
+  readonly backButton: Locator;
+  readonly checkinButton: Locator;
+  readonly checkinHeading: Locator;
+  readonly cpfInput: Locator;
+  readonly searchUserButton: Locator;
+  readonly colorStatus: Locator;
+  readonly nameField: Locator;
+  readonly packageField: Locator;
+  readonly paymentField: Locator;
+  readonly valueField: Locator;
+  readonly birthdayField: Locator;
+  readonly accomodationField: Locator;
+  readonly subAccomodationField: Locator;
+  readonly roomField: Locator;
+  readonly foodField: Locator;
+  readonly extraMealsField: Locator;
+  readonly checkinSelect: Locator;
+  readonly updateCheckinButton: Locator;
+  readonly checkinSuccessfulyUpdatedToast: Locator;
+  readonly checkinUpdatedToFalseToast: Locator;
+  readonly checkinItems: Locator[];
 
   constructor(readonly page: Page) {
-    
+    this.registeredButton = page.getByText('Inscritos', { exact: true });
+    this.registeredHeading = page.getByRole('heading', { name: 'Gerenciamento de Inscritos' });
+    this.checkinFieldInTheCampersTable = page.locator('table tbody tr:first-child td:nth-of-type(29)');
+
+    this.backButton = page.getByRole('button', { name: 'Voltar' });
+    this.checkinButton = page.getByText('Check-in', { exact: true });
+    this.checkinHeading = page.getByRole('heading', { name: 'Check-in de Usuário' });
+    this.cpfInput = page.getByRole('spinbutton', { name: 'CPF do Usuário:' });
+    this.searchUserButton = page.getByRole('button', { name: 'Buscar Usuário' });
+    this.colorStatus = page.locator('.checkin-color-status-wrapper__line');
+    this.nameField = page.getByText('Nome: Yuri Galeno Pinheiro');
+    this.packageField = page.getByText('Pacote: PACOTE 14 -');
+    this.paymentField = page.getByText('Forma de Pagamento: Cartão de');
+    this.valueField = page.getByText('Valor do Pagamento: 208');
+    this.birthdayField = page.getByText('Data de Nascimento: 23/10/');
+    this.accomodationField = page.getByText('Acomodação: Outra Acomodacao');
+    this.subAccomodationField = page.getByText('Sub Acomodação: Outra');
+    this.roomField = page.getByText('Quarto: Não alocado');
+    this.foodField = page.getByText('Alimentação: Almoco e jantar');
+    this.extraMealsField = page.getByText('Dias de Refeição Extra:');
+    this.checkinItems = [
+      this.colorStatus,
+      this.nameField,
+      this.packageField,
+      this.paymentField,
+      this.valueField,
+      this.birthdayField,
+      this.accomodationField,
+      this.subAccomodationField,
+      this.roomField,
+      this.foodField,
+      this.extraMealsField,
+    ];
+    this.checkinSelect = page.getByLabel('Check-in realizado?');
+    this.updateCheckinButton = page.getByRole('button', { name: 'Atualizar Check-in' });
+    this.checkinSuccessfulyUpdatedToast = page.getByText('Check-in realizado com sucesso');
+    this.checkinUpdatedToFalseToast = page.getByText('Status de Check-in atualizado para não checado');
+  }
+
+  async backAndGoToCheckinPage() {
+    await this.backButton.click();
+    await this.checkinButton.click();
+  }
+
+  async backAndGoToRegisteredPage() {
+    await this.backButton.click();
+    await this.registeredButton.click();
+  }
+
+  async findUser() {
+    await this.cpfInput.fill('03561746462');
+    await this.searchUserButton.click();
+  }
+
+  async checkinSelectToggle(option: string) {
+    await this.checkinSelect.selectOption(option);
+    await this.updateCheckinButton.click();
   }
 }
--- tests/test-cases/checkin/checkin.spec.ts
+++ tests/test-cases/checkin/checkin.spec.ts
@@ -1,6 +1,41 @@
-import { expect } from '@playwright/test';
-import { checkinTest as test } from 'tests/fixtures/checkinTest';
+import { expect, mergeTests } from '@playwright/test';
+import { checkinTest } from 'tests/fixtures/checkinTest';
+import { authenticationTest } from 'tests/fixtures/authenticationTest';
+
 import { testsConfig } from 'tests/tests.config';
 
+const test = mergeTests(authenticationTest, checkinTest);
+
 test.describe('Checkin flow', () => {
+  test('Verify if it is possible to check in a user', async ({ authentication, checkin }) => {
+    await authentication.goToAdminPage();
+
+    const adminPassword = testsConfig.users.testUser;
+    await authentication.login(adminPassword);
+
+    await checkin.registeredButton.click();
+    await expect(checkin.registeredHeading).toBeVisible();
+    await expect(checkin.checkinFieldInTheCampersTable).toHaveText('Não');
+    await checkin.backAndGoToCheckinPage();
+    await expect(checkin.checkinHeading).toBeVisible();
+    await checkin.findUser();
+
+    for (let i = 0; i < checkin.checkinItems.length; i++) {
+      const section = checkin.checkinItems[i];
+      await expect(section).toBeVisible();
+    }
+
+    await checkin.checkinSelectToggle('true');
+    await expect(checkin.checkinSuccessfulyUpdatedToast).toBeVisible();
+
+    await checkin.backAndGoToRegisteredPage();
+    await expect(checkin.registeredHeading).toBeVisible();
+    await expect(checkin.checkinFieldInTheCampersTable).toHaveText(/Sim/);
+    await checkin.backAndGoToCheckinPage();
+
+    await expect(checkin.checkinHeading).toBeVisible();
+    await checkin.findUser();
+    await checkin.checkinSelectToggle('false');
+    await expect(checkin.checkinUpdatedToFalseToast).toBeVisible();
+  });
 });
