68b3925de7ef7374161370124d78d3521363720e
Álvaro Leal <alvaro.leal@liferay.com>
2025-02-13 11:30:18 -0300

    Update feedback component to delete all feedbacks

--- src/Pages/Admin/Feedback/index.jsx
+++ src/Pages/Admin/Feedback/index.jsx
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react';
-import { Container, Table } from 'react-bootstrap';
+import { Container, Table, Modal, Button } from 'react-bootstrap';
+import { toast } from 'react-toastify';
+import PropTypes from 'prop-types';
 import './style.scss';
+import { registerLog } from '@/fetchers/userLogs';
 import fetcher from '@/fetchers/fetcherWithCredentials';
 import scrollUp from '@/hooks/useScrollUp';
 import Loading from '@/components/Global/Loading';
@@ -19,9 +22,10 @@ const TABLE_HEADERS = [
   'Campo Aberto',
 ];
 
-const AdminFeedback = () => {
+const AdminFeedback = ({ loggedUsername }) => {
   const [feedbacks, setFeedbacks] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
 
   scrollUp();
 
@@ -39,9 +43,39 @@ const AdminFeedback = () => {
     fetchFeedbacks();
   }, []);
 
+  const handleDeleteFeedbacks = async () => {
+    try {
+      const response = await fetcher.delete('feedback');
+
+      if (response.status === 200) {
+        toast.success('Todos os feedbacks foram deletados com sucesso');
+        registerLog(`Deletou todos os feedbacks`, loggedUsername);
+        setShowDeleteModal(false);
+      }
+    } catch (error) {
+      console.error('Error adding data:', error);
+      toast.error('Erro ao deletar feedbacks');
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <Container fluid>
-      <AdminHeader pageName="Gerenciamento de Feedbacks" sessionTypeIcon="feedback" iconSize={80} fill="#204691" />
+      <AdminHeader
+        pageName="Gerenciamento de Feedbacks"
+        sessionTypeIcon="feedback"
+        iconSize={80}
+        fill="#204691"
+        showHeaderTools
+        headerToolsClassname="table-tools__right-buttons-generic flex-sm-column flex-md-row  d-flex gap-2"
+        headerToolsTypeButton="warning"
+        headerToolsOpenModal={() => setShowDeleteModal(true)}
+        headerToolsButtonIcon="danger"
+        headerToolsButtonSize={20}
+        headerToolsButtonFill={'#000'}
+        headerToolsButtonName="Deletar Todos Feedbacks"
+      />
       <Table striped bordered hover responsive className="custom-table mt-3">
         <thead>
           <tr>
@@ -62,9 +96,36 @@ const AdminFeedback = () => {
           ))}
         </tbody>
       </Table>
+
+      <Modal show={showDeleteModal} onHide={() => setShowDeleteModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            <b>Confirmar Exclusão</b>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Tem certeza que deseja excluir todos os Feedbacks?{' '}
+          <em>
+            <b>Essa ação é irreversível!</b>
+          </em>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowDeleteModal(false)}>
+            Cancelar
+          </Button>
+          <Button variant="danger" onClick={handleDeleteFeedbacks}>
+            Deletar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
       <Loading loading={loading} />
     </Container>
   );
+};
+
+AdminFeedback.propTypes = {
+  loggedUsername: PropTypes.string,
 };
 
 export default AdminFeedback;
