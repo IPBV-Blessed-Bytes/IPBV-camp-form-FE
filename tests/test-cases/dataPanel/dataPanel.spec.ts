cfdac68c5571feb6a727ee690925d7ddca3715fc
Álvaro Leal <alvaro.leal@liferay.com>
2025-02-12 23:49:48 -0300

    Delete data panel tests

--- tests/test-cases/dataPanel/dataPanel.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect } from '@playwright/test';
-import { dataPanelTest as test } from 'tests/fixtures/dataPanelTest';
-import { testsConfig } from 'tests/tests.config';
-
-test.describe('Data panel flow', () => {
-});
