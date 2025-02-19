4335c420969b157dc8ece3031d1554e1ed8a4ea6
Álvaro Leal <alvaro.leal@liferay.com>
2025-02-19 11:13:31 -0300

    Delete user logs and vacancies number tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 import { config as authentication } from 'tests/test-cases/authentication/config';
 import { config as common } from 'tests/test-cases/common/config';
-// import { config as form } from 'tests/test-cases/form/config';
+import { config as form } from 'tests/test-cases/form/config';
 // import { config as camperTable } from 'tests/test-cases/camperTable/config';
 import { config as adminHome } from 'tests/test-cases/adminHome/config';
 // import { config as ride } from 'tests/test-cases/ride/config';
@@ -11,8 +11,6 @@ import { config as room } from 'tests/test-cases/room/config';
 import { config as checkRegistration } from 'tests/test-cases/checkRegistration/config';
 import { config as faq } from 'tests/test-cases/faq/config';
 import { config as extraMeals } from 'tests/test-cases/extraMeals/config';
-// import { config as userLogs } from 'tests/test-cases/userLogs/config';
-// import { config as vacanciesNumber } from 'tests/test-cases/vacanciesNumber/config';
 import { config as userCreation } from 'tests/test-cases/userCreation/config';
 import { config as checkin } from 'tests/test-cases/checkin/config';
 
@@ -36,7 +34,7 @@ export default defineConfig({
   projects: [
     authentication,
     common,
-    // form,
+    form,
     // camperTable,
     adminHome,
     // ride,
@@ -45,8 +43,6 @@ export default defineConfig({
     checkRegistration,
     faq,
     extraMeals,
-    // userLogs,
-    // vacanciesNumber,
     userCreation,
     checkin,
   ],
--- tests/test-cases/vacanciesNumber/vacanciesNumber.spec.ts
+++
@@ -1,6 +0,0 @@
-import { expect } from '@playwright/test';
-import { vacanciesNumberTest as test } from 'tests/fixtures/vacanciesNumberTest';
-import { testsConfig } from 'tests/tests.config';
-
-test.describe('Vacancies number flow', () => {
-});
