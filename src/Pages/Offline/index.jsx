3bdfb6812b01288d72583b5166f4c006c0cf6a82
Álvaro Leal <alvaro.leal@liferay.com>
2025-03-05 21:56:20 -0300

    Update offline component to show form off context

--- src/Pages/Offline/index.jsx
+++ src/Pages/Offline/index.jsx
@@ -3,7 +3,6 @@ import { useNavigate } from 'react-router-dom';
 import './style.scss';
 import Header from '@/components/Global/Header';
 import Footer from '@/components/Global/Footer';
-import Icons from '@/components/Global/Icons';
 
 const Offline = () => {
   const navigate = useNavigate();
@@ -18,52 +17,15 @@ const Offline = () => {
               <div className="form__success text-center">
                 <div className="form__success__title">
                   <h2>
-                    <b>As inscrições para o acampamento de 2025 foram encerradas. Nos vemos no sábado em Garanhuns!</b>
+                    <b>As inscrições para o acampamento de 2026 começarão em breve. Aguarde!</b>
                   </h2>
                 </div>
-                <div className="offline-buttons mb-4">
-                  <div className="offline-buttons__verify">
-                    <Card.Title>Verifique sua inscrição:</Card.Title>
-                    <Card.Text>
-                      Insira seu CPF e data de nascimento para verificar seus dados de inscrição, caso esteja inscrito.
-                      Acompanhantes, pacote, status de pagamento, alimentação, contato de carona, valor pago, entre
-                      outros.
-                    </Card.Text>
-                    <div className="offline-buttons__verify-button">
-                      <button className="verify-registration-button" onClick={() => navigate('/verificacao')}>
-                        Verificar Inscrição&nbsp;
-                        <Icons className="info-icons" typeIcon="refresh" iconSize={25} fill={'#000'} />
-                      </button>
-                    </div>
-                  </div>
-
-                  <hr className="horizontal-line" />
-
-                  <div className="offline_buttons-faq">
-                    <Card.Title>Perguntas Frequentes:</Card.Title>
-                    <Card.Text>
-                      Visualize aqui algumas perguntas frequentes e suas respostas. Já foi a dúvida de outros, pode ser
-                      a sua também!
-                    </Card.Text>
-                    <div className="offline-buttons__verify-button">
-                      <button className="verify-registration-button" onClick={() => navigate('/perguntas')}>
-                        Perguntas Frequentes&nbsp;
-                        <Icons className="info-icons" typeIcon="question" iconSize={25} fill={'#000'} />
-                      </button>
-                    </div>
-                  </div>
-                </div>
+                <p className="form__success__message"></p>
                 <p className="form__success__contact">
                   <b>
                     Qualquer dúvida nos contacte no telefone da secretaria para mais informações. <br />
                     (81) 9839-0194 (Whatsapp)
                   </b>
-                </p>
-                <p>
-                  <em>
-                    Informamos que cancelamentos devem ser solicitados com, no máximo, 7 dias de antecedência da data do
-                    acampamento, com reembolso limitado a 50% do valor total.
-                  </em>
                 </p>
                 <small className="mt-5">
                   <em>Igreja Presbiteriana de Boa Viagem</em>
@@ -73,7 +35,7 @@ const Offline = () => {
           </Card.Body>
         </Card>
       </div>
-      <Footer onAdminClick={() => navigate('/admin')} />
+      <Footer onAdminClick={() => navigate('/admin')}/>
     </>
   );
 };
