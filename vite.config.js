23c0e483ba7c0f8f8700e6f1547d5f6c5e5ae82f
Álvaro Leal <alvaro.leal@liferay.com>
2025-05-23 14:29:15 -0300

    Remove vitest

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-/// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -9,16 +8,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  test: {
-    silent: true,
-    globals: true,
-    environment: 'jsdom',
-    coverage: {
-      reporter: ['text'],
-    },
-    setupFiles: ['./vitest.setup.js'],
-    include: ['**/*.test.{js,jsx}'],
-  },
   plugins: [
     react(),
     eslint({
