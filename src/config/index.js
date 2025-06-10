304fa63bf51a4314f2c680e6fec1249bf09d901f
Álvaro Leal <alvaro.leal@liferay.com>
2025-06-10 10:07:45 -0300

    Add global test url

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,11 @@
 export const JWT_LOCAL_STORAGE_KEY = 'token_jwt';
 
+export const TEST_URL = 'http://35.85.144.78:8080';
+
 export const BASE_URL = 'http://35.85.144.78:8080';
 
 export const LOGIN_ROUTE = '/admin';
 
 export const USER_STORAGE_KEY = 'user-data';
 
-export const USER_STORAGE_ROLE = 'user-role'
+export const USER_STORAGE_ROLE = 'user-role';
