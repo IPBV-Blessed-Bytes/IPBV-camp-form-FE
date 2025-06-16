fbf088bcd43597f733eed87f97d018cf9b89b50a
Álvaro Leal <alvaro.leal@liferay.com>
2025-06-16 13:55:37 -0300

    Add cart provider in the main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { CartProvider } from 'react-use-cart';
 import AuthProvider from './hooks/useAuth/AuthProvider.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles/GlobalStyle.scss'
+import './styles/GlobalStyle.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -22,7 +23,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       theme="colored"
     />
     <AuthProvider>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </AuthProvider>
   </BrowserRouter>,
 );
