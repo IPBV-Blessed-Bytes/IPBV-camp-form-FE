20f8dc36617b6fa3cbc852c4ec8d6a8551d08e1a
Álvaro Leal <alvaro.leal@liferay.com>
2025-06-16 14:01:53 -0300

    Add products mock

---
+++ src/components/cart/products.js
@@ -0,0 +1,49 @@
+export const products = [
+  // Hospedagem
+  {
+    id: 'host-college',
+    name: 'Colégio',
+    price: 0,
+    image: '/img/hospedagem.png',
+    category: 'Hospedagem',
+  },
+  {
+    id: 'host-external',
+    name: 'Externo',
+    price: 0,
+    image: '/img/hospedagem.png',
+    category: 'Hospedagem',
+  },
+  {
+    id: 'host-seminario',
+    name: 'Seminário',
+    price: 500,
+    image: '/img/hospedagem.png',
+    category: 'Hospedagem',
+  },
+
+  // Transporte
+  {
+    id: 'bus-yes',
+    name: 'Com Ônibus',
+    price: 200,
+    image: '/img/bus.png',
+    category: 'Transporte',
+  },
+  {
+    id: 'bus-no',
+    name: 'Sem Ônibus',
+    price: 0,
+    image: '/img/bus.png',
+    category: 'Transporte',
+  },
+
+  // Alimentação
+  {
+    id: 'food-complete',
+    name: 'Alimentação Completa (Café, Almoço e Jantar)',
+    price: 300,
+    image: '/img/food.png',
+    category: 'Alimentação Completa',
+  },
+];
