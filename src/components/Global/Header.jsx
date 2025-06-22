3e3f46f769f9d0d3c60ac2ed303f14e9752ca91a
Álvaro Leal <alvaro.leal@liferay.com>
2025-06-22 07:16:30 -0300

    Delete cart modal in the header component and add some new header steps

--- src/components/Global/Header.jsx
+++ src/components/Global/Header.jsx
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { enumSteps } from '@/utils/constants';
 import '../Style/style.scss';
 import Breadcrumb from 'react-bootstrap/Breadcrumb';
 import Icons from './Icons';
-import { Button, Modal } from 'react-bootstrap';
-import Cart from './Cart';
+import { Button } from 'react-bootstrap';
 
-const Header = ({ currentStep, goBackToStep, formSubmitted, showNavMenu, formValues }) => {
-  const headerSteps = ['Início', 'Informações Pessoais', 'Contato', 'Pacote', 'Refeição Extra', 'Revisão', 'Pagamento'];
-  const [showCartModal, setShowCartModal] = useState(false);
+const Header = ({ currentStep, goBackToStep, formSubmitted, showNavMenu }) => {
+  const headerSteps = [
+    'Início',
+    'Informações Pessoais',
+    'Contato',
+    'Pacote',
+    'Refeição Extra',
+    'Revisão',
+    'Items',
+    'Pagamento',
+  ];
+
   const navigateTo = useNavigate();
 
   const handleStepChange = (newStep) => {
@@ -18,9 +27,7 @@ const Header = ({ currentStep, goBackToStep, formSubmitted, showNavMenu, formVal
       return;
     }
 
-    if (formSubmitted) {
-      return;
-    }
+    if (formSubmitted) return;
 
     if (newStep <= currentStep) {
       goBackToStep(newStep);
@@ -34,6 +41,7 @@ const Header = ({ currentStep, goBackToStep, formSubmitted, showNavMenu, formVal
           ACAMPAMENTO IPBV 2026
         </a>
       </h2>
+
       {showNavMenu && (
         <Breadcrumb className="mt-4">
           {headerSteps.map((step, index) => (
@@ -64,18 +72,9 @@ const Header = ({ currentStep, goBackToStep, formSubmitted, showNavMenu, formVal
         </Breadcrumb>
       )}
 
-      <Button className="cart-btn" onClick={() => setShowCartModal(true)}>
+      <Button className="cart-btn" onClick={() => goBackToStep(enumSteps.beforePayment)}>
         <Icons typeIcon="cart" iconSize={30} fill={'#0066cc'} />
       </Button>
-
-      <Modal show={showCartModal} onHide={() => setShowCartModal(false)} size="lg">
-        <Modal.Header closeButton>
-          <Modal.Title>Resumo do Carrinho</Modal.Title>
-        </Modal.Header>
-        <Modal.Body className='p-0'>
-          <Cart formValues={formValues} />
-        </Modal.Body>
-      </Modal>
     </header>
   );
 };
@@ -85,7 +84,7 @@ Header.propTypes = {
   goBackToStep: PropTypes.func.isRequired,
   formSubmitted: PropTypes.bool,
   showNavMenu: PropTypes.bool,
-  formValues: PropTypes.object
+  savedUsers: PropTypes.array,
 };
 
 export default Header;
