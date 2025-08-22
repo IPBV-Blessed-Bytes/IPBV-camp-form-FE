14c6ee67fd10382fbad8039c32c139acf0caa2ed
Álvaro Leal <alvaro.leal@liferay.com>
2025-08-22 10:16:53 -0300

    Add simple arrow icon

--- src/components/Global/Icons.jsx
+++ src/components/Global/Icons.jsx
@@ -265,6 +265,25 @@ const Icons = ({ className, iconSize, onClick, typeIcon, fill, stroke }) => {
         </svg>
       )}
 
+      {typeIcon === 'simple-arrow' && (
+        <svg
+          className={className}
+          onClick={onClick}
+          height={iconSize ? iconSize + 'px' : '30px'}
+          fill={fill || ''}
+          viewBox="-19.04 0 75.804 75.804"
+        >
+          <g id="Group_65" data-name="Group 65" transform="translate(-831.568 -384.448)">
+            <path
+              id="Path_57"
+              data-name="Path 57"
+              d="M833.068,460.252a1.5,1.5,0,0,1-1.061-2.561l33.557-33.56a2.53,2.53,0,0,0,0-3.564l-33.557-33.558a1.5,1.5,0,0,1,2.122-2.121l33.556,33.558a5.53,5.53,0,0,1,0,7.807l-33.557,33.56A1.5,1.5,0,0,1,833.068,460.252Z"
+              fill={fill || ''}
+            />
+          </g>
+        </svg>
+      )}
+
       {typeIcon === 'person' && (
         <svg
           className={className}
