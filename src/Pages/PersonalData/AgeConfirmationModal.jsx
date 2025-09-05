2865339b4795466b263a4008527a55260c043fc1
Álvaro Leal <alvaro.leal@liferay.com>
2025-09-05 07:04:00 -0300

    Update age confirmation modal style

--- src/Pages/PersonalData/AgeConfirmationModal.jsx
+++ src/Pages/PersonalData/AgeConfirmationModal.jsx
@@ -1,5 +1,6 @@
 import { Button, Modal } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import Icons from '@/components/Global/Icons';
 
 const AgeConfirmationModal = ({
   showModal,
@@ -9,9 +10,10 @@ const AgeConfirmationModal = ({
   restoreScrollWhenMobile,
 }) => {
   return (
-    <Modal show={showModal} onHide={handleCancelAge} onExited={restoreScrollWhenMobile}>
-      <Modal.Header closeButton>
-        <Modal.Title>
+    <Modal className="custom-modal" show={showModal} onHide={handleCancelAge} onExited={restoreScrollWhenMobile}>
+      <Modal.Header closeButton className="custom-modal__header--confirm">
+        <Modal.Title className="d-flex align-items-center gap-2">
+          <Icons typeIcon="checked" iconSize={25} fill={'#057c05'} />
           <b>Confirmação de Idade</b>
         </Modal.Title>
       </Modal.Header>
@@ -19,10 +21,10 @@ const AgeConfirmationModal = ({
         A sua idade no período do acampamento será de <strong>{currentAge} anos</strong>. Confirma essa idade?
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="secondary" onClick={handleCancelAge}>
+        <Button variant="outline-secondary" onClick={handleCancelAge}>
           Cancelar
         </Button>
-        <Button variant="primary" onClick={handleConfirmAge}>
+        <Button variant="info" className='btn-confirm' onClick={handleConfirmAge}>
           Confirmar
         </Button>
       </Modal.Footer>
