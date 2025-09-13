b692fb02b23268ca9545eef982445412c1f17de1
Álvaro Leal <alvaro.leal@liferay.com>
2025-09-13 02:54:07 -0300

    Update producs to fetch from endpoint

--- src/Pages/Packages/utils/products.js
+++ src/Pages/Packages/utils/products.js
@@ -1,4 +1,6 @@
-export const products = [
+import fetcher from '@/fetchers';
+
+export let products = [
   {
     id: 'host-college-collective',
     name: 'Colégio Quarto Coletivo',
@@ -23,7 +25,7 @@ export const products = [
   {
     id: 'host-seminario',
     name: 'Seminário',
-    price: 280,
+    price: 0,
     description: 'Quarto do seminário São José',
     category: 'Hospedagem',
   },
@@ -37,7 +39,7 @@ export const products = [
   {
     id: 'bus-yes',
     name: 'Com Ônibus',
-    price: 200,
+    price: 0,
     description: 'Ônibus disponibilizado pela igreja',
     category: 'Transporte',
   },
@@ -51,7 +53,7 @@ export const products = [
   {
     id: 'food-complete',
     name: 'Alimentação Completa',
-    price: 220,
+    price: 0,
     description: 'Café da manhã, almoço e jantar inclusos',
     category: 'Alimentação',
   },
@@ -62,7 +64,7 @@ export const products = [
     description: 'Sem refeições',
     category: 'Alimentação',
   },
-  // {
+   // {
   //   id: 'food-external',
   //   name: 'Alimentação Parcial',
   //   price: 240,
@@ -70,3 +72,37 @@ export const products = [
   //   category: 'Alimentação',
   // },
 ];
+
+export const loadProducts = async () => {
+  try {
+    const response = await fetcher.get('lots');
+    const data = response.data;
+
+    if (data.lots && data.lots.length > 0) {
+      const lot = data.lots[0];
+      products = products.map((product) => {
+        switch (product.id) {
+          case 'host-seminario':
+            return { ...product, price: Number(lot.price.seminary) };
+          case 'host-college-collective':
+          case 'host-college-family':
+          case 'host-college-camping':
+            return { ...product, price: Number(lot.price.school) };
+          case 'host-external':
+            return { ...product, price: Number(lot.price.otherAccomodation) };
+          case 'bus-yes':
+            return { ...product, price: Number(lot.price.bus) };
+          case 'food-complete':
+            return { ...product, price: Number(lot.price.food) };
+          default:
+            return product;
+        }
+      });
+    }
+
+    return products;
+  } catch (error) {
+    console.error('Erro ao carregar os produtos:', error);
+    return products;
+  }
+};
