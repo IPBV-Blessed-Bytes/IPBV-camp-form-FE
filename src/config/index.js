b89a392256fbeb233efe76ea89f63b167f1f0f1d
Álvaro Leal <alvaro.leal@liferay.com>
2025-09-15 21:11:21 -0300

    Update endpoints prod url

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ export const JWT_LOCAL_STORAGE_KEY = 'token_jwt';
 
 export const TEST_URL = 'http://3.17.23.226:8080';
 
-export const BASE_URL = 'http://3.17.23.226:8080';
+export const BASE_URL = 'https://www.blessedbytes-campform.camp';
 
 export const LOGIN_ROUTE = '/admin';
 
