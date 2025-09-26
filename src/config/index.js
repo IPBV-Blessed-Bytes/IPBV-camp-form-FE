7efb409335cc692cef695c1f8f2b8bcfe32c29ec
Álvaro Leal <alvaro.leal@liferay.com>
2025-09-26 12:06:28 -0300

    Update endpoint base url

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,7 @@ export const PROD_URL = 'https://www.blessedbytes-campform.camp';
 
 export const TEST_URL = 'http://18.219.157.234:8080';
 
-export const BASE_URL = 'http://18.219.157.234:8080';
+export const BASE_URL = 'https://www.blessedbytes-campform.camp';
 
 export const LOGIN_ROUTE = '/admin';
 
