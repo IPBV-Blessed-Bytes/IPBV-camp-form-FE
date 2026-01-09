2e45e1419076bfa4dc14b0d3b7ceaede129abddc
Álvaro Leal <alvaro.leal@liferay.com>
2026-01-09 08:55:56 -0300

    Add logic to use formContext from authContext provider in the sideButtons component

--- src/components/Admin/SideButtons.jsx
+++ src/components/Admin/SideButtons.jsx
@@ -2,11 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import PropTypes from 'prop-types';
 import { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '@/hooks/useAuth/AuthProvider';
 import Icons from '@/components/Global/Icons';
 
 const SideButtons = ({ primaryPermission, secondaryPermission }) => {
   const [showSettingsButtons, setShowSettingsButtons] = useState(false);
   const settingsButtonsRef = useRef(null);
+  const { formContext } = useContext(AuthContext);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -38,33 +40,62 @@ const SideButtons = ({ primaryPermission, secondaryPermission }) => {
       )}
 
       {primaryPermission && (
-        <button className="data-panel-btn" onClick={() => navigate('/admin/painel')} title="Painel de Dados">
+        <button
+          className="data-panel-btn"
+          onClick={formContext === 'maintenance' ? () => navigate('/dev/painel') : () => navigate('/admin/painel')}
+          title="Painel de Dados"
+        >
           <Icons typeIcon="chart" iconSize={45} fill={'#fff'} />
         </button>
       )}
 
       <div className={`settings-floating-buttons ${showSettingsButtons ? 'show' : ''}`}>
-        <button className="settings-message-button" onClick={() => navigate('/admin/logs')}>
+        <button
+          className="settings-message-button"
+          onClick={formContext === 'maintenance' ? () => navigate('/dev/logs') : () => navigate('/admin/logs')}
+        >
           Logs de Usuários&nbsp;
           <Icons className="settings-icons" typeIcon="logs" iconSize={25} fill={'#fff'} />
         </button>
 
-        <button className="settings-message-button" onClick={() => navigate('/admin/vagas')}>
+        <button
+          className="settings-message-button"
+          onClick={formContext === 'maintenance' ? () => navigate('/dev/vagas') : () => navigate('/admin/vagas')}
+        >
           Controle de Vagas&nbsp;
           <Icons className="settings-icons" typeIcon="camp" iconSize={25} fill={'#fff'} />
         </button>
 
-        <button className="settings-message-button" onClick={() => navigate('/admin/lotes')}>
+        <button
+          className="settings-message-button"
+          onClick={formContext === 'maintenance' ? () => navigate('/dev/lotes') : () => navigate('/admin/lotes')}
+        >
           Controle de Lotes&nbsp;
           <Icons className="settings-icons" typeIcon="calendar" iconSize={22} fill={'#fff'} />
         </button>
 
-        <button className="settings-message-button" onClick={() => navigate('/admin/usuarios')}>
+        <button
+          className="settings-message-button"
+          onClick={formContext === 'maintenance' ? () => navigate('/dev/usuarios') : () => navigate('/admin/usuarios')}
+        >
           Controle de Usuários&nbsp;
           <Icons className="settings-icons" typeIcon="add-person" iconSize={25} fill={'#fff'} />
         </button>
 
-        <button className="settings-message-button" onClick={() => navigate('/admin/contexto')}>
+        <button
+          className="settings-message-button"
+          onClick={
+            formContext === 'maintenance' ? () => navigate('/dev/pulseiras') : () => navigate('/admin/pulseiras')
+          }
+        >
+          Pulseiras e Parceiros&nbsp;
+          <Icons className="settings-icons" typeIcon="wristband" iconSize={25} fill={'#fff'} />
+        </button>
+
+        <button
+          className="settings-message-button"
+          onClick={formContext === 'maintenance' ? () => navigate('/dev/contexto') : () => navigate('/admin/contexto')}
+        >
           Contexto do Formulário&nbsp;
           <Icons className="settings-icons" typeIcon="form-context" iconSize={25} fill={'#fff'} />
         </button>
@@ -74,6 +105,7 @@ const SideButtons = ({ primaryPermission, secondaryPermission }) => {
 };
 
 SideButtons.propTypes = {
+  formContext: PropTypes.string,
   primaryPermission: PropTypes.bool,
   secondaryPermission: PropTypes.bool,
 };
