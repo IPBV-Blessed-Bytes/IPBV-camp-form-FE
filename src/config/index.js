74fed1baf65a3ff3ee8df7811fd02fd9a8571e5e
Álvaro Leal <alvaro.leal@liferay.com>
2026-01-09 08:48:51 -0300

    Update test url and add form context key

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ export const JWT_LOCAL_STORAGE_KEY = 'token_jwt';
 
 export const PROD_URL = 'https://www.blessedbytes-campform.camp';
 
-export const TEST_URL = 'http://18.219.157.234:8080';
+export const TEST_URL = 'http://3.144.213.175:8080';
 
 export const BASE_URL = 'https://www.blessedbytes-campform.camp';
 
@@ -11,3 +11,5 @@ export const LOGIN_ROUTE = '/admin';
 export const USER_STORAGE_KEY = 'user-data';
 
 export const USER_STORAGE_ROLE = 'user-role';
+
+export const FORM_CONTEXT_KEY = 'formContext';
