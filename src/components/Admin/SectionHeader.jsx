ed7fe113b7ae72a7f5c94a89a540b6278b810b65
Álvaro Leal <alvaro.leal@liferay.com>
2026-05-29 10:18:12 -0300

    Add section header component

---
+++ src/components/Admin/SectionHeader.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+const SectionHeader = ({ title, count }) => (
+  <div className="admin-section-header">
+    <h4 className="admin-section-header__title">{title}</h4>
+    {typeof count === 'number' && (
+      <span className="admin-section-header__count">{count} itens</span>
+    )}
+    <div className="admin-section-header__line" />
+  </div>
+);
+
+SectionHeader.propTypes = {
+  title: PropTypes.string.isRequired,
+  count: PropTypes.number,
+};
+
+export default SectionHeader;
