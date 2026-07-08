1e0e48e17531063fb74b24c9b52832a0baecca10
Álvaro Leal <alvaro.leal@liferay.com>
2026-07-08 09:08:19 -0300

    Remove unused import

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { lazy, Suspense, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 
