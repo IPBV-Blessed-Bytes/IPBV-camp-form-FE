e0b7f5261aee8bfdccfd51add1ab7da237f60676
Álvaro Leal <alvaro.leal@liferay.com>
2026-08-19 10:02:13 -0300

    Add edit modal icon

--- src/components/Global/Icons.jsx
+++ src/components/Global/Icons.jsx
@@ -228,6 +228,23 @@ const Icons = ({ className, iconSize, onClick, typeIcon, fill, stroke }) => {
         </svg>
       )}
 
+      {typeIcon === 'edit-modal' && (
+        <svg
+          className={className}
+          onClick={onClick}
+          height={iconSize ? iconSize + 'px' : '30px'}
+          fill='transparent'
+          stroke={fill}
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+        >
+          <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
+          <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z" />
+        </svg>
+      )}
+
       {typeIcon === 'delete' && (
         <svg
           className={className}
