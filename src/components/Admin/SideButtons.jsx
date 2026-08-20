8dda0c6a92bd3fbb62468771eba6b5f7cfd2f9f6
Álvaro Leal <alvaro.leal@liferay.com>
2026-08-20 17:36:29 -0300

    Reorder admin settings sections

--- src/components/Admin/SideButtons.jsx
+++ src/components/Admin/SideButtons.jsx
@@ -59,14 +59,6 @@ const SideButtons = ({ secondaryPermission }) => {
 
         <button
           className="settings-message-button"
-          onClick={formStage === 'maintenance' ? () => navigate('/dev/faq') : () => navigate('/admin/faq')}
-        >
-          Perguntas Frequentes&nbsp;
-          <Icons className="settings-icons" typeIcon="question" iconSize={25} fill={'#fff'} />
-        </button>
-
-        <button
-          className="settings-message-button"
           onClick={formStage === 'maintenance' ? () => navigate('/dev/logs') : () => navigate('/admin/logs')}
         >
           Logs de Usuários&nbsp;
@@ -87,6 +79,14 @@ const SideButtons = ({ secondaryPermission }) => {
         >
           Papéis e Permissões&nbsp;
           <Icons className="settings-icons" typeIcon="feedback" iconSize={25} fill={'#fff'} />
+        </button>
+
+        <button
+          className="settings-message-button"
+          onClick={formStage === 'maintenance' ? () => navigate('/dev/faq') : () => navigate('/admin/faq')}
+        >
+          Perguntas Frequentes&nbsp;
+          <Icons className="settings-icons" typeIcon="question" iconSize={25} fill={'#fff'} />
         </button>
 
         <button
