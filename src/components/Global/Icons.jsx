1aa591f807730bf7dc372509e868e8257a447781
Álvaro Leal <alvaro.leal@liferay.com>
2026-08-27 18:57:03 -0300

    Add exit icon

--- src/components/Global/Icons.jsx
+++ src/components/Global/Icons.jsx
@@ -1920,6 +1920,23 @@ const Icons = ({ className, iconSize, onClick, typeIcon, fill, stroke }) => {
           </g>
         </svg>
       )}
+
+      {typeIcon === 'exit' && (
+        <svg
+          className={'form-icons ' + className}
+          height={iconSize ? iconSize + 'px' : '30px'}
+          width={iconSize ? iconSize + 'px' : '30px'}
+          onClick={onClick}
+          fill={fill || '#000000'}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          stroke={stroke || ''}
+        >
+          <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+          <polyline points="16 17 21 12 16 7"></polyline>
+          <line x1="21" x2="9" y1="12" y2="12"></line>
+        </svg>
+      )}
     </>
   );
 };
