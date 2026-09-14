5147a2a16c1c61ed280ccdfdd92b115e9c57e25a
Álvaro Leal <alvaro.leal@liferay.com>
2026-09-14 15:30:36 -0300

    Change request UX: datepicker, masks, justification, admin diff

--- src/services/me.js
+++ src/services/me.js
@@ -15,8 +15,11 @@ export const getMyRegistration = async (id) => {
   return data;
 };
 
-export const createChangeRequest = async (id, payload) => {
-  const { data } = await authFetcher.post(`/me/registrations/${id}/change-request`, payload);
+export const createChangeRequest = async (id, payload, justification) => {
+  const { data } = await authFetcher.post(`/me/registrations/${id}/change-request`, {
+    data: payload,
+    justification,
+  });
   return data;
 };
 
