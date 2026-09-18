564ed78be6bf64a1ccf8fd68b5250dd27bd80fe6
Álvaro Leal <alvaro.leal@liferay.com>
2026-09-18 14:17:52 -0300

    feat: show the FAQ chatbot widget on the institutional page

--- src/Pages/Institutional/index.jsx
+++ src/Pages/Institutional/index.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import Icons from '@/components/Global/Icons';
 import Loading from '@/components/Global/Loading';
 import Footer from '@/components/Global/Footer';
+import ChatbotWidget from '@/components/Global/ChatbotWidget';
 import { useFormState } from '@/contexts/FormStateContext';
 import { getInstitutionalContent, institutionalImageUrl } from '@/services/institutional';
 import { DEFAULT_INSTITUTIONAL_CONTENT, GALLERY_TONES, INSTITUTIONAL_NAV } from '@/config/institutionalContent';
@@ -251,6 +252,8 @@ const Institutional = () => {
       </section>
 
       <Footer handleAdminClick={handleAdminClick} />
+
+      <ChatbotWidget />
     </div>
   );
 };
